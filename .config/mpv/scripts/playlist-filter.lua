13f9494edc93418d601c54079cb4a9095cf93a15
zsugabubus <zsugabubus@users.noreply.github.com>
2020-11-21 10:55:53 +0100

    mpv: add script playlist-filter.lua

---
+++ .config/mpv/scripts/playlist-filter.lua
@@ -0,0 +1,20 @@
+mp.observe_property('playlist', nil, function()
+	local playlist = mp.get_property_native('playlist')
+
+	if #playlist <= 1 then
+		return
+	end
+
+	for i=#playlist, 1, -1 do
+		local entry = playlist[i]
+		local s = entry.filename:lower()
+		if s:match('^sample') or
+		   s:match('[-.]sample') or
+		   s:match('%.exe$') or
+		   s:match('%.nfo$') or
+		   s:match('%.txt$') then
+			mp.msg.info('removing playlist entry', s)
+			mp.command(('playlist-remove %d'):format(i - 1))
+		end
+	end
+end)
