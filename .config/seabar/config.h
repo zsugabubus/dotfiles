35baf4d1309cc81448b4f5c85bb3d5f7f9b424e8
zsugabubus <zsugabubus@users.noreply.github.com>
2020-11-28 16:05:13 +0100

    seabar: add config

---
+++ .config/seabar/config.h
@@ -0,0 +1,42 @@
+static char const BLOCK_SEP[] = " ";
+static char const GROUP_SEP[] = " \xe2\x9d\x98 ";
+
+#define NAMED_DIR(hash) "$(zsh -c '. $ZDOTDIR/hashes.zsh;print " hash "')"
+
+static Block BLOCKS[] =
+{
+	{ 1, block_battery, "BAT0", ANSI_RGB(123, 63, 0, "%F\xf0\x9f\xa6\x87 %s, %p[%P] (%l)") },
+
+	/* { 8, block_backlight, "backlight/intel_backlight", "LVDS %i %p" },
+	{ 8, block_backlight, "leds/dell::kbd_backlight", "KBD %i %p %b/%B" }, */
+
+	{ 2, block_datetime, NULL, " %a, %_d %b \e[1m%H∶%M\e[m" },
+
+	{ 3, block_alsa, "Master", ANSI_RGB(26, 83, 159, "%i%d") },
+
+	{ 4, block_cpu, "", ANSI_RGB(234, 15, 72, " " ANSI_BOLD("%p")) },
+	{ 4, block_memory, NULL, ANSI_RGB(234, 85, 34, " %u/%t (" ANSI_BOLD("%p") ")") },
+
+	{ 5, block_fs, NAMED_DIR("~S"), "%i%n: %r%a" },
+	{ 5, block_fs, NAMED_DIR("~N"), "%i%n: %r%a" },
+	{ 5, block_fs, NAMED_DIR("~m"), "%i%n: %r%a" },
+
+/* #define NET_UP_FORMAT ANSI_RGB(92, 169, 58, "%U%i%a  %R @ " ANSI_BOLD ("%r") "  %T @ " ANSI_BOLD("%t")) \ */
+#define NET_UP_FORMAT "%U%i%a  %R @ \e[1;38;5;34m%r\e[m  %T @ \e[1;38;5;33m%t\e[m" \
+
+	{ 6, block_net, "enp10s0", NET_UP_FORMAT "\t" "%n (down)"},
+	{ 6, block_net, "wlp2s0", NET_UP_FORMAT },
+
+#undef NET_UP_FORMAT
+
+	{ 7, block_uptime, NULL, "♥ %t" },
+};
+
+#undef NAMED_DIR
+
+static Block *blocks = BLOCKS;
+static size_t num_blocks = ARRAY_SIZE(BLOCKS);
+
+static void
+init(void)
+{ }
