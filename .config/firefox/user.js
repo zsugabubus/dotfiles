b72f4c6d5afe55cbb6e3ad7ca01d1cf41a2dafeb
zsugabubus <zsugabubus@users.noreply.github.com>
2021-01-16 11:57:43 +0100

    firefox: Update user.js

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -49,7 +49,7 @@ user_pref('browser.search.suggest.enabled', false);
 user_pref('browser.search.update', false);
 user_pref('browser.sessionhistory.max_entries', 5);
 user_pref('browser.sessionstore.enabled', false);
-user_pref('browser.sessionstore.restore_on_demand', false);
+user_pref('browser.sessionstore.restore_on_demand', true);
 user_pref('browser.sessionstore.resume_from_crash', false);
 user_pref('browser.sessionstore.resume_session', false);
 user_pref('browser.shell.checkDefaultBrowser', false);
