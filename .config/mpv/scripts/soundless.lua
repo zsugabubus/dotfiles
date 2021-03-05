4e6f7b3fdedec00f8f336f97e9af417e4c7fb0d5
zsugabubus <zsugabubus@users.noreply.github.com>
2021-03-05 17:33:21 +0100

    mpv: Update scripts

--- .config/mpv/scripts/soundless.lua
+++ .config/mpv/scripts/soundless.lua
@@ -1,17 +1,26 @@
 -- For dumpass people who do not get why they cannot hear anything.
 local osd = mp.create_osd_overlay("ass-events")
 
+function get_icon(align, icon)
+	return "{\\an" .. align .. "\\1a&H40\\fscx200\\fscy200\\fnmpv-osd-symbols}" .. icon
+end
+
 function update(_, _)
-	osd.data = "{\\an4\\1a&H40\\fscx200\\fscy200\\fnmpv-osd-symbols}"
+	osd.data = ""
+
 	if mp.get_property_bool('pause') then
-		osd.data = osd.data .. "\238\128\130"
-	elseif mp.get_property_bool('mute') then
-		osd.data = osd.data .. "\238\132\138"
-	else
-		osd:remove()
-		return
+		osd.data = osd.data ..  get_icon(4, "\238\128\130") .. "\n"
 	end
-	osd:update()
+
+	if mp.get_property_bool('mute') then
+		osd.data = osd.data ..  get_icon(6, "\238\132\138") .. "\n"
+	end
+
+	if osd.data == "" then
+		osd:remove()
+	else
+		osd:update()
+	end
 end
 
 mp.observe_property('mute', 'bool', update)
