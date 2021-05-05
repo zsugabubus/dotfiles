881917948dee2eaf57216a2d8a1dc958ed2cd04d
zsugabubus <zsugabubus@users.noreply.github.com>
2021-05-05 08:56:10 +0200

    seabar: Tweak config

--- .config/seabar/config.h
+++ .config/seabar/config.h
@@ -1,10 +1,11 @@
 static char const BLOCK_SEP[] = " ";
-static char const GROUP_SEP[] = " \xe2\x9d\x98 ";
+static char const GROUP_SEP[] = /* " \xe2\x9d\x98 " */ "\n\e[K";
 
 #define NAMED_DIR(hash) "$(zsh -c '. $ZDOTDIR/hashes.zsh;print " hash "')"
 
 static Block BLOCKS[] =
 {
+	/* { 1, block_seabar, NULL, "%t" }, */
 	{ 1, block_battery, "BAT0", ANSI_RGB(123, 63, 0, "%F\xf0\x9f\xa6\x87 %s, %p[%P] (%l)") },
 
 	/* { 8, block_backlight, "backlight/intel_backlight", "LVDS %i %p" },
@@ -12,7 +13,8 @@ static Block BLOCKS[] =
 
 	{ 2, block_datetime, NULL, " %a, %_d %b \e[1m%H∶%M\e[m" },
 
-	{ 3, block_alsa, "Master", ANSI_RGB(26, 83, 159, "%i%d") },
+	{ 3, block_alsa, "default,Master", ANSI_RGB(26, 83, 159, "%P%i%d (%p)") },
+	{ 3, block_alsa, "default,Digital", ANSI_RGB(26, 83, 159, "%C%i%p") },
 
 	{ 4, block_cpu, "", ANSI_RGB(234, 15, 72, " " ANSI_BOLD("%p")) },
 	{ 4, block_memory, NULL, ANSI_RGB(234, 85, 34, " %u/%t (" ANSI_BOLD("%p") ")") },
@@ -20,6 +22,8 @@ static Block BLOCKS[] =
 	{ 5, block_fs, NAMED_DIR("~S"), "%i%n: %r%a" },
 	{ 5, block_fs, NAMED_DIR("~N"), "%i%n: %r%a" },
 	{ 5, block_fs, NAMED_DIR("~m"), "%i%n: %r%a" },
+	{ 5, block_fs, NAMED_DIR("~e/silicon"), "%i%n: %r%a" },
+	{ 5, block_fs, NAMED_DIR("~e/archive"), "%i%n: %r%a" },
 
 /* #define NET_UP_FORMAT ANSI_RGB(92, 169, 58, "%U%i%a  %R @ " ANSI_BOLD ("%r") "  %T @ " ANSI_BOLD("%t")) \ */
 #define NET_UP_FORMAT "%U%i%a  %R @ \e[1;38;5;34m%r\e[m  %T @ \e[1;38;5;33m%t\e[m" \
