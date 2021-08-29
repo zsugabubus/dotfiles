cef6041c6d744489d067d35a9e4a91c40c0d0f03
zsugabubus <zsugabubus@users.noreply.github.com>
2021-08-29 16:49:16 +0200

    tmux: Use fzf config from Zsh

--- .config/zsh/theme-light.zsh
+++ .config/zsh/theme-light.zsh
@@ -1,3 +1,3 @@
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=245'
-FZF_DEFAULT_OPTS+=' --color=gutter:-1,pointer:214,hl+:reverse:226,hl:reverse:-1,spinner:240'
+FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --color=gutter:-1,pointer:214,hl+:reverse:226,hl:reverse:-1,spinner:240"
 ZSH_OPEN_TAGS_FOREGROUND='16'
