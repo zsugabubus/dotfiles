e3bb893b5d7af8a5e96a2ba68dee18b81f7e25dc
zsugabubus <zsugabubus@users.noreply.github.com>
2021-10-04 18:38:34 +0200

    mpv/osc-visibility: Remove unused script

--- .config/mpv/scripts/osc-visibility.lua
+++
@@ -1,9 +0,0 @@
---[[ function on_changed(_, count)
-  mp.command('script-message osc-visibility ' ..
-    (count ~= 1 and 'always' or 'auto'))
-  if count > 1 then
-    mp.unobserve_property(on_changed)
-  end
-end
-
-mp.observe_property('playlist-count', 'number', on_changed) ]]
