958789f922eb64a640e3d325bfa5138061aec241
zsugabubus <zsugabubus@users.noreply.github.com>
2021-10-08 17:23:22 +0200

    mpv/title: Use osd.lua

--- .config/mpv/scripts/title/main.lua
+++ .config/mpv/scripts/title/main.lua
@@ -1,24 +1,13 @@
-local osd = mp.create_osd_overlay('ass-events')
+local osd = require('osd')
 local visible = false
 
 osd.z = 10
-
-local function ass_escape(s)
-	local x = s:gsub('[\\{]', '\\%0')
-	return x
-end
-
-local function osd_append(...)
-	for _, s in ipairs({...}) do
-		osd.data[#osd.data + 1] = s
-	end
-end
 
 function update()
 	local style = '\\c&H00ffFF\\bord2\\fscx70\\fscy70}'
 	osd.data = {}
 
-	osd_append('\n{\\an2', style)
+	osd:append('\n{\\an2', style)
 	local artist = mp.get_property_native('metadata/by-key/Artist', nil)
 	local title = mp.get_property_native('metadata/by-key/Title', nil) or
 	              mp.get_property_native('media-title', nil)
@@ -26,15 +15,15 @@ function update()
 	if artist and title then
 		local version = mp.get_property_native('metadata/by-key/Version', nil)
 
-		osd_append(ass_escape(artist), ' - ', '{\\b1}', ass_escape(title), '{\\b0}')
+		osd:append(osd.ass_escape(artist), ' - ', '{\\b1}', osd.ass_escape(title), '{\\b0}')
 
 		if version then
-			osd_append(' (', ass_escape(version), ')')
+			osd:append(' (', osd.ass_escape(version), ')')
 		end
 	elseif title then
-		osd_append(ass_escape(title))
+		osd:append(osd.ass_escape(title))
 	else
-		osd_append(ass_escape(mp.get_property_native('path')))
+		osd:append(osd.ass_escape(mp.get_property_native('path')))
 	end
 
 	for _, track in ipairs(mp.get_property_native('track-list')) do
@@ -44,14 +33,14 @@ function update()
 		if track.type == 'video' then
 			local pars = mp.get_property_native('video-params')
 			if pars then
-				osd_append('\n{\\an3', style,
+				osd:append('\n{\\an3', style,
 					('%s%dx%d'):format(track.albumart and '[P] ' or '', pars.w, pars.h))
 			end
 			break
 		elseif track.type == 'audio' then
 			local pars = mp.get_property_native('audio-params')
 			if pars then
-				osd_append('\n{\\an3', style,
+				osd:append('\n{\\an3', style,
 					('%s %s'):format(track.codec, pars['hr-channels']))
 			end
 		end
@@ -60,10 +49,10 @@ function update()
 
 	local duration = mp.get_property('duration')
 	if duration then
-		osd_append('\n{\\an3', style, (' %02d:%02d'):format(duration / 60, duration % 60))
+		osd:append('\n{\\an3', style, (' %02d:%02d'):format(duration / 60, duration % 60))
 	end
 
-	osd_append('\n{\\an1', style,
+	osd:append('\n{\\an1', style,
 		mp.get_property('playlist-pos'),
 		'/',
 		mp.get_property('playlist-count'))
