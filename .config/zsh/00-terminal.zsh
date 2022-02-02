fca73d213c427e9da02f934ab55ceaea1cbe28cf
zsugabubus <zsugabubus@users.noreply.github.com>
2022-02-02 17:20:11 +0100

    zsh: Set terminal colors through escape sequences

--- .config/zsh/00-terminal.zsh
+++ .config/zsh/00-terminal.zsh
@@ -1,13 +1,6 @@
 autoload -Uz add-zsh-hook
 
 if [[ $TERM == linux ]]; then
-	# See console_codes(4).
-	printf '\e%G' # UTF-8.
-	typeset -a PALETTE=(
-		000000 ec407a 8bc34a ffa726 2196f3 9575cd 00bcd4 c4c4c4
-		617d8a ec407a 9ccc65 ffb74d 42a5f5 b39ddb 26c6da ffffff
-	)
-	for i ({1..16}); printf '\e]P%x%s' $((i - 1)) ${PALETTE[$i]}
 	setterm --blank 1 --powerdown 1 --powersave powerdown
 fi
 
--- .config/zsh/05-exports.zsh
+++ .config/zsh/05-exports.zsh
@@ -9,8 +9,6 @@ export MANPAGER=$EDITOR\ +Man!
 
 export RIPGREP_CONFIG_PATH=$HOME/.config/ripgrep/ripgreprc
 
-export FZF_DEFAULT_OPTS='--tiebreak=end --layout=reverse --no-mouse --no-multi --hscroll-off=13' # --bind alt+space:jump-accept
-
 export GPG_TTY=$TTY
 
 export MAIL=~/mail
