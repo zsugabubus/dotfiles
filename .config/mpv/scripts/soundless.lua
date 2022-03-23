240322d992cc4f9b47c80b2f6ce6fe02631ec76a
zsugabubus <zsugabubus@users.noreply.github.com>
2022-03-23 03:01:43 +0100

    mpv/soundless: Modify icon style

--- .config/mpv/scripts/soundless.lua
+++ .config/mpv/scripts/soundless.lua
@@ -1,12 +1,14 @@
 -- For dumpass people who do not get why they cannot hear anything.
 local osd = mp.create_osd_overlay('ass-events')
 
-function icon(align, icon)
+function icon(alpha, align, icon)
 	return table.concat{
-		"{\\an",
+		'{\\an',
 		align,
-		"\\1a&H40\\fscx200\\fscy200\\fnmpv-osd-symbols}",
-		icon
+		'\\1a&H',
+		alpha,
+		'\\fscx250\\fscy250\\fnmpv-osd-symbols}',
+		icon,
 	}
 end
 
@@ -14,11 +16,11 @@ function update()
 	osd.data = {}
 
 	if mp.get_property_bool('pause') then
-		table.insert(osd.data, icon(4, '\238\128\130'))
+		table.insert(osd.data, icon('10', 4, '\238\128\130'))
 	end
 
 	if mp.get_property_bool('mute') then
-		table.insert(osd.data, icon(6, '\238\132\138'))
+		table.insert(osd.data, icon('50', 6, '\238\132\138'))
 	end
 
 	if 0 < #osd.data then
