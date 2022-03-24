5571c1bd568ab865c3e56022ae79e01452a93481
zsugabubus <zsugabubus@users.noreply.github.com>
2022-03-24 09:25:36 +0100

    zsh/zhere: Remove useless aliases
    There/ is not expanded nor completed.

--- .config/zsh/Plugins/zhere.zsh
+++ .config/zsh/Plugins/zhere.zsh
@@ -1,10 +1,7 @@
-alias -g There='~[here]'
-alias -g Tmux='~[tmux-pane]'
-
 typeset -g _ZSH_HERE_FILE=$XDG_RUNTIME_DIR/.zhere
 function here() {
 	print >$_ZSH_HERE_FILE -- $PWD
-	printf 'There=%q\n' There
+	printf '~[here]=%q\n' ~[here]
 }
 
 add-zsh-hook -Uz zsh_directory_name tmux_directory_name
