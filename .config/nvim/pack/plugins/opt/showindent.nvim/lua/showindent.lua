443343b9dc3c1f9aa7ddcf534e8e396e690c4ff2
zsugabubus <zsugabubus@users.noreply.github.com>
2022-04-23 10:52:34 +0200

    nvim/showindent.nvim: Use newer API + Fix indent string

--- .config/nvim/pack/plugins/opt/showindent.nvim/lua/showindent.lua
+++ .config/nvim/pack/plugins/opt/showindent.nvim/lua/showindent.lua
@@ -1,51 +1,54 @@
-local ns = vim.api.nvim_create_namespace('showindent')
+local V = vim.api
+local ns = V.nvim_create_namespace('showindent')
 local attached = {}
 
 local function detach_from_buffer(buffer)
 	if (buffer or 0) == 0 then
-		buffer = vim.api.nvim_get_current_buf()
+		buffer = V.nvim_get_current_buf()
 	end
 	attached[buffer] = nil
-	vim.api.nvim_buf_clear_namespace(buffer, ns, 0, -1)
+	V.nvim_buf_clear_namespace(buffer, ns, 0, -1)
 end
 
 local function update_lines(buffer, fromline, toline)
-	vim.api.nvim_buf_clear_namespace(buffer, ns, fromline, toline)
+	V.nvim_buf_clear_namespace(buffer, ns, fromline, toline)
 
-	local listchars = vim.api.nvim_get_option('listchars')
+	local listchars = V.nvim_get_option('listchars')
 	local space = '·'
-	for _space in listchars:gmatch('space:([^,]+)') do
-		space = _space
+	for m in listchars:gmatch('space:([^,]+)') do
+		space = m
 	end
 	local tab = ' │'
-	for _tab in listchars:gmatch('tab:([^,]+)') do
-		tab = _tab
+	for m in listchars:gmatch('tab:([^,]+)') do
+		tab = m
 	end
-	local tabta, tabab = tab:match("^([\x01-\x7f\xc2-\xf4][\x80-\xbf]*)(.*)$")
+	local tab_head, tab_rep = tab:match("^([\x01-\x7f\xc2-\xf4][\x80-\xbf]*)(.*)$")
 
-	local expandtab = vim.api.nvim_buf_get_option(buffer, 'expandtab')
-	local tabstop = vim.api.nvim_buf_get_option(buffer, 'tabstop')
-	local shiftwidth = vim.api.nvim_buf_get_option(buffer, 'shiftwidth')
+	local expandtab = V.nvim_buf_get_option(buffer, 'expandtab')
+	local tabstop = V.nvim_buf_get_option(buffer, 'tabstop')
+	local shiftwidth = V.nvim_buf_get_option(buffer, 'shiftwidth')
 	if shiftwidth == 0 then
 		shiftwidth = tabstop
 	end
-	local indentexpr = vim.api.nvim_buf_get_option(buffer, 'indentexpr')
+	local indentexpr = V.nvim_buf_get_option(buffer, 'indentexpr')
 	local eval = false
 	local lnumoff = 0
 	if indentexpr ~= '' then
 		eval = true
-	elseif vim.api.nvim_buf_get_option(buffer, 'cindent') then
+	elseif V.nvim_buf_get_option(buffer, 'cindent') then
 		indentexpr = 'cindent'
-	elseif vim.api.nvim_buf_get_option(buffer, 'lisp') then
+	elseif V.nvim_buf_get_option(buffer, 'lisp') then
 		indentexpr = 'lispindent'
-	elseif vim.api.nvim_buf_get_option(buffer, 'smartindent') then
+	elseif V.nvim_buf_get_option(buffer, 'smartindent') then
 		lnumoff = -1
 		indentexpr = 'indent'
 	else
 		return
 	end
 
-	for index, line in ipairs(vim.api.nvim_buf_get_lines(buffer, fromline, toline, false)) do
+	for index, line in ipairs(
+		V.nvim_buf_get_lines(buffer, fromline, toline, false)
+	) do
 		-- Skip line if not blank.
 		if #line > 0 then
 			goto next_line
@@ -56,12 +59,15 @@ local function update_lines(buffer, fromline, toline)
 
 		local indent
 		if eval then
-			-- Evaluate 'indentexpr' in sandbox.
-			vim.api.nvim_set_vvar('lnum', lnum1)
-			vim.api.nvim_command('sandbox let v:errmsg = ('..indentexpr:gsub('\n', ' ')..')')
-			indent = vim.api.nvim_get_vvar('errmsg')
+			V.nvim_set_vvar('lnum', lnum1)
+			V.nvim_command(('sandbox let v:errmsg = (%s)'):format(
+				indentexpr:gsub('\n', ' ')
+			))
+			indent = V.nvim_get_vvar('errmsg')
 		else
-			indent = vim.api.nvim_call_function(indentexpr, {lnum1 + lnumoff})
+			indent = V.nvim_call_function(indentexpr, {
+				lnum1 + lnumoff
+			})
 		end
 
 		-- Skip if not number.
@@ -71,17 +77,20 @@ local function update_lines(buffer, fromline, toline)
 		end
 
 		local indentstr
-
-		-- FIXME: Read values from 'listchars'.
 		if not expandtab then
-			indentstr = (((tabstop == shiftwidth and tabab or space):rep(shiftwidth - 1)..tabta):rep(indent / shiftwidth))
+			indentstr = (
+				(tab_head .. tab_rep:rep(tabstop - 1))
+					:rep(math.floor(indent / tabstop)) ..
+				space:rep(indent % tabstop)
+			)
 		else
-			indentstr = space:rep(indent - 1)
+			indentstr = space:rep(indent)
 		end
 
-		vim.api.nvim_buf_set_virtual_text(buffer, ns, lnum0, {
-			{indentstr, 'Whitespace'}
-		}, {})
+		V.nvim_buf_set_extmark(buffer, ns, lnum0, 0, {
+			virt_text={{indentstr, 'Whitespace'}},
+			virt_text_pos='overlay',
+		})
 
 		::next_line::
 	end
@@ -90,16 +99,21 @@ end
 return {
 	attach_to_buffer=function(buffer)
 		if (buffer or 0) == 0 then
-			buffer = vim.api.nvim_get_current_buf()
+			buffer = V.nvim_get_current_buf()
 		end
 		if attached[buffer] then
 			return
 		end
 
-		update_lines(buffer, 0, vim.api.nvim_buf_line_count(buffer) - 1)
+		update_lines(buffer, 0, V.nvim_buf_line_count(buffer) - 1)
 
-		vim.api.nvim_buf_attach(buffer, false, {
+		V.nvim_buf_attach(buffer, false, {
 			on_lines=function(_, changedtick, firstline, lastline, new_lastline)
+				-- Changing indentation of last line (likely) changes indentation of
+				-- empty lines following it.
+				new_lastline = V.nvim_call_function('nextnonblank', {
+					new_lastline + 1
+				})
 				update_lines(buffer, firstline, new_lastline)
 				return not attached[buffer]
 			end,
