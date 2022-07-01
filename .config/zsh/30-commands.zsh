ccf31d3402a818379170a4ab88b661c892f58b91
zsugabubus <zsugabubus@users.noreply.github.com>
2022-07-01 23:32:39 +0200

    zsh: Update commands
    - Move small files from Misc/.

--- .config/zsh/30-commands.zsh
+++ .config/zsh/30-commands.zsh
@@ -43,7 +43,7 @@ alias g='git'
 alias git='noglob git'
 alias grep='grep --color=auto'
 alias hh='HOME=$PWD'
-alias ht='HOME=${TMPDIR:-/tmp}'
+alias ht='HOME=${TMPDIR:-/tmp} XDG_RUNTIME_DIR=${TMPDIR:-/tmp}'
 alias iftop='sudo -E iftop'
 alias info='info --vi-keys'
 alias info_all='() { info --subnodes "$@" }'
@@ -59,6 +59,7 @@ alias pkill='pkill -x'
 alias pm='progress -M'
 alias readelf='readelf -W'
 alias rm='rm -dI --one-file-system'
+alias systemctl='noglob systemctl'
 alias tree='tree-color'
 alias utop='top -u $USER'
 alias vlock_all='nice -20 vlock -a'
@@ -73,8 +74,6 @@ compdef '_files -W ~/.config/passwords' pl
 alias asm='gcc -fno-stack-protector -fno-asynchronous-unwind-tables -S'
 alias configure_make='./configure && make'
 alias cpd='() { rsync -aihPv -- $^*/ }'
-alias du..='du --apparent-size -chd 1 . | sort -h'
-alias du.='du --apparent-size -csh . | sort -h'
 alias fm='findmnt --real -o TARGET,SOURCE,FSTYPE,SIZE,USED,AVAIL,USE%,OPTIONS'
 alias ipt='sudo iptables -xvL --line-numbers | sed '"'"'s/^Chain \(\S\+\)/Chain \x1b[1m\1\x1b[0m/'"'"
 alias make='nice -n15 make -j2'
@@ -82,6 +81,22 @@ alias oct='od -tu1'
 alias term='$TERMINAL >/dev/null &disown'
 alias topp='() { top -p${^$(pidof $1)} }'
 alias upnp='upnpc -u "http://router.lan:5000/rootDesc.xml"'
+
+function du() {
+	command du -bchd 1 ${@:-.} | sort -h
+}
+
+function du.() {
+	du *(.)
+}
+
+function im() {
+	nsxiv ${@:-.}
+}
+
+function vid() {
+	mpv (#i)*.(mp4|gif|webm|mkv)(.)
+}
 
 function catrm() {
 	local f
@@ -307,8 +322,13 @@ function difforig() {
 }
 compdef '_files -g "*.orig"' difforig
 
-alias www='() { if [[ 1 == $# ]]; then www $1; else www - : -- tar -cf - $*; fi } '
-alias timer='() { ( sleep ${1:-5m} && ~/doc/cuckoo-clock.mp3 ) &! }'
+function head.() {
+	head *(.)
+}
+
+function timer() {
+	( sleep ${1:-5m} && ffplay ~/doc/cuckoo-clock.mp3 ) &
+}
 
 autoload -U zmv
 alias sdir='noglob __zmv -M'
@@ -467,7 +487,13 @@ function js-beautify() {
 }
 
 function print_composekeys() {
-	less "/usr/share/X11/locale/$(grep --max-count=1 "${LANG%.*}.UTF-8\$" /usr/share/X11/locale/locale.dir | cut -d/ -f1)/Compose"
+	{
+		cat "/usr/share/X11/locale/$(
+			grep --max-count=1 "${LANG%.*}.UTF-8\$" /usr/share/X11/locale/locale.dir |
+			cut -d/ -f1
+		)/Compose"
+		xmodmap -pke
+	} | e
 }
 
 function rs() {
@@ -542,7 +568,7 @@ function spek() {
 	for f; do
 		() {
 			ffmpeg -i $2 -lavfi showspectrumpic=s=1024x512:color=rainbow -c:v png -f image2pipe -y $1 &&
-			feh $1
+			nsxiv $1
 		} =() $f
 	done
 }
@@ -573,6 +599,112 @@ function files() {
 	print -rc $files
 }
 alias	files='noglob files'
+
+function checksum() {
+	local algo
+	for algo in md5 sha1 sha256 sha224 sha384 sha512; do
+		print -rP "%B%F{blue}::%f Checking ${algo}sum...%b"
+		find -H -maxdepth 2 -iname ${algo}sum.txt -o -iname "*.${algo}" \
+			-execdir ${algo}sum -c {} \;
+	done | ${PAGER:-less}
+}
+
+function fkill() {
+	emulate -L zsh
+	setopt pipe_fail
+
+	local pid
+	local args=()
+	(( EUID )) && args+=(-u $EUID)
+	ps $args -o pid,pcpu,state,time,command |
+	awk 'NR<2 { print; next } { print | "sort -rgk2" }' |
+	fzf -m --header-lines=1 -q "'" |
+	awk '{ print $1 }' |
+	xargs -r kill -${1:-9}
+}
+
+function gdbrun() {
+	local exe=${1?}
+	shift
+	gdb -quiet $exe \
+		-ex 'set confirm off' \
+		-ex 'handle SIG32 noprint nostop' \
+		-ex 'handle SIG34 noprint nostop' \
+		-ex "run ${(jj j)${(qq)@}}"
+}
+
+function license() {
+	case $1 in
+	gpl3|)
+		curl -Lo COPYING 'https://www.gnu.org/licenses/gpl-3.0.txt' ;;
+
+	lgpl)
+		curl -Lo COPYING 'https://www.gnu.org/licenses/gpl-3.0.txt' \
+					-o COPYING.LESSER 'https://www.gnu.org/licenses/lgpl-3.0.txt' ;;
+
+	wtfpl)
+		curl -Lo COPYING 'http://www.wtfpl.net/txt/copying' ;;
+
+	un)
+		curl -Lo UNLICENSE 'https://unlicense.org/UNLICENSE' ;;
+
+	*)
+		printf >&2 "unknown license kind: %s\n" "$1"
+		return 1 ;;
+	esac
+}
+
+function speedtest() {
+	emulate -L zsh
+	trap return INT
+
+	() {
+		print -P "\n%B%F{blue}::%f Upload %F{blue}::%f%b"
+		curl -T /dev/zero http://speedtest.tele2.net/upload.php -o /dev/null
+	}
+	() {
+		print -P "%B%F{blue}::%f Download %F{blue}::%f%b"
+		curl http://speedtest.tele2.net/1GB.zip -o /dev/null
+	}
+}
+
+
+function emv() {
+	(( $# )) || set -- *(N)
+
+	local f=
+	for f; do
+		local dst=$f
+		vared -e dst &&
+		mkdir -p -- $dst:h &&
+		mv -i -v -- "$f" "$dst"
+	done
+}
+
+function eln() {
+	emulate -L zsh
+	zmodload -F zsh/stat b:zstat
+	zmodload zsh/system
+
+	local f=
+	typeset -A stat
+
+	for f; do
+		stat=()
+		if zstat -LNH stat -F '' -- $f && [[ ${stat[mode]:0:1} != 'l' ]]; then
+			printf '%s: not a link\n' "$f"
+			continue
+		fi
+
+		local target=$stat[link]
+		vared -ep '${(q)f:t} -> ' target &&
+		ln -sfn "$target" "$f"
+	done
+}
+
+function pyman() {
+	python -c "import $1; help($1)"
+}
 
 autoload -Uz ${:-$ZDOTDIR/Misc/*(N.:t)}
 source ${:-$ZDOTDIR/Plugins/*(N)}
