0933be912be34ba1a021fb5141b0d54dec20031b
zsugabubus <zsugabubus@users.noreply.github.com>
2022-10-20 21:30:56 +0200

    zsh: Simplify auto-{ls,git}

--- .config/zsh/80-shell.zsh
+++ .config/zsh/80-shell.zsh
@@ -15,18 +15,13 @@ add-zsh-hook chpwd auto-push
 
 local function auto-ls() {
 	(( __zfiles_active )) && return
-	# Auto-ls small directories only.
-	local -A stat
-	zstat -sNH stat -F '' -- . 2>/dev/null &&
-	(( ${stat[size]} <= 4096 )) &&
-	l
+	(( $(zstat +size .) <= 4096 )) && l
 }
 add-zsh-hook chpwd auto-ls
 
 local function auto-git() {
 	(( __zfiles_active )) && return
-	test -d .git || return
 	[[ $PWD == ~ ]] && return
-	git zsh-status
+	test -d .git && git zsh-status
 }
 add-zsh-hook chpwd auto-git
