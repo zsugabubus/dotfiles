2149a96a7a91fdcbbb1542bdad25e44721a68d3c
zsugabubus <zsugabubus@users.noreply.github.com>
2022-10-21 15:55:12 +0200

    zsh: Improve completions

--- .config/zsh/20-completion.zsh
+++ .config/zsh/20-completion.zsh
@@ -28,17 +28,18 @@ setopt list_rows_first
 setopt nomenu_complete # Only second <Tab> completes menu.
 unsetopt list_types
 
-# See: ZSHCOMPWID(1) "COMPLETION MATCHING CONTROL"
-zstyle ':completion::complete:*::' matcher-list \
-	'' 'm:{[:lower:]-_}={[:upper:]_-} l:|=* r:|[-.]=* r:|[-_./]|/=* r:|=*'
-
-# zstyle ':completion::complete:*' menu # no-select yes
 zstyle ':completion::complete:*' verbose yes
 zstyle ':completion::complete:*' file-sort modification reverse follow
 # Place every tag in the same-named group.
 zstyle ':completion::complete:*' group-name ''
-
 zstyle ':completion::complete:*' list-colors ${(s.:.)LS_COLORS_ONLY}
+# (1) Exact begin.
+# (2) Smart-case begin.
+# (3) X/Y X*/Y
+# (4) --> Begin match anywhere.
+# (5) *l*a*z*y*
+zstyle ':completion::complete:*::' matcher-list \
+	'' +'m:{[:lower:]-_}={[:upper:]_-} r:|[-_./]=*' +'l:|=* r:|?=**'
 
 # Complete files only once per line.
 zstyle ':completion::complete:*:other-files' ignore-line other
@@ -48,39 +49,30 @@ zstyle ':completion::complete:*' single-ignored show
 # Always perform completion on <Tab>.
 zstyle ':completion::*' insert-tab false
 
-# zstyle ':completion::complete:e:*' menu select
-zstyle ':completion::complete:(mp|mpv):*' ignored-patterns '*.aria2' '(#i)**/*sample*' '*.(txt|nfo)'
-zstyle ':completion::complete:e:*' ignored-patterns '*.(o|d|out)'
-
 zstyle ':completion::complete:*' list-dirs-first true
 zstyle ':completion::complete:(cat|cp|rm|nvim):*' file-patterns \
 	'%p(^-/):other-files:files %p(-/):directories:directories'
 
-zstyle ':completion::complete:-tilde-::' group-order named-directories path-directories expand
-
 zstyle ':completion::complete:-command-:*' file-patterns \
 	'*(#q-*):executables:"exacutable file" *(-/):directories:directory'
-# zstyle ':completion::complete:-command-:*' file-patterns \
-#		'%p(^-/):globbed-files:globbed-files %p(-/):globbed-directories:globbed-directories'
 zstyle ':completion::complete:-command-:*:functions' ignored-patterns '_*'
 zstyle ':completion::complete:-command-::parameters' ignored-patterns '[A-Z]*'
-zstyle ':completion::complete:-command-::' tag-order 'local-directories' 'users'
+zstyle ':completion::complete:-command-::' tag-order 'local-directories directories' '! users' '-'
 zstyle ':completion::complete:-command-::' group-order \
-	executables directories local-directories suffix-aliases directory-stack path-directories builtins functions commands
+	directories local-directories suffix-aliases directory-stack path-directories builtins functions
 
-# `cd`
 zstyle ':completion::complete:cd::' ignore-parents parent pwd
-zstyle ':completion::complete:cd::' tag-order 'local-directories' 'directory-stack path-directories named-directories' 'users'
+zstyle ':completion::complete:cd::' tag-order 'local-directories' '! users' '-'
 zstyle ':completion::complete:cd::' group-order local-directories named-directories
 
-# `man`
+zstyle ':completion::complete:-tilde-::' tag-order 'named-directories' '! users' '-'
+zstyle ':completion::complete:-tilde-::' group-order named-directories path-directories expand
+
 zstyle ':completion::complete:man:manuals' separate-sections true
 
-# `kill`
-zstyle ':completion::complete:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=1'
-zstyle ':completion:*:processes'                command "ps -u $USER -o pid,state,command -w -w"
+zstyle ':completion::complete:*:processes' command "ps fo pid,stat,tname,start_time,pcpu,pmem,time,command -u $USER"
+zstyle ':completion::complete:*:processes' list-colors '=(#b) #([0-9]#)*=0=1'
+zstyle ':completion::complete:*:processes' sort false
+zstyle ':completion::complete:*:processes' menu yes select
 
-# `killall`
-# zstyle ':completion::complete:killall:*' menu yes select
-# zstyle ':completion::complete:killall:*' force-list always
-# zstyle ':completion:*:processes-names' command "ps -u $USER --no-headers -o args,pid,state,command -w -w"
+zstyle ':completion::complete:mpv:argument-rest:' group-order files directories
