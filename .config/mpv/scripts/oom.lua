51dd9c88469eced5855d258a18a4ccee32f586f0
zsugabubus <zsugabubus@users.noreply.github.com>
2023-02-21 18:21:58 +0100

    mpv/oom: Add script to configure oom_score_adj

---
+++ .config/mpv/scripts/oom.lua
@@ -0,0 +1,11 @@
+local utils = require 'mp.utils'
+local pid = utils.getpid()
+
+local function oom_score_adj(n)
+	local path = ('/proc/%d/%s'):format(pid, 'oom_score_adj')
+	local f = assert(io.open(path, 'wb'))
+	assert(f:write(tostring(n)))
+	assert(f:close())
+end
+
+oom_score_adj(900)
