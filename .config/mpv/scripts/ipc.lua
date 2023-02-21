ad9bad5dc9f23f3e508c89c44440a348fd7e2103
zsugabubus <zsugabubus@users.noreply.github.com>
2023-02-21 18:28:25 +0100

    mpv/ipc: Add script to auto set input-ipc-server

---
+++ .config/mpv/scripts/ipc.lua
@@ -0,0 +1,16 @@
+local mp = require 'mp'
+
+if (
+	mp.get_property_native('input-ipc-server') == '' and
+	mp.get_property_native('input-ipc-client') == ''
+) then
+	local options = {
+		name = '',
+	}
+	require 'mp.options'.read_options(options)
+
+	local pid = require 'mp.utils'.getpid()
+	local path = ('/tmp/mpv%s%s'):format(options.name, pid)
+	mp.msg.info('Automatic server:', path)
+	mp.set_property_native('input-ipc-server', path)
+end
