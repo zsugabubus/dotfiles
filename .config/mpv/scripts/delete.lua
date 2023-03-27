811e9c34661ac9be294a95ab4638b14e41146dcb
zsugabubus <zsugabubus@users.noreply.github.com>
2023-03-27 02:33:00 +0200

    mpv/delete: Correctly handle time-pos == nil

--- .config/mpv/scripts/delete.lua
+++ .config/mpv/scripts/delete.lua
@@ -1,6 +1,6 @@
 local utils = require('mp.utils')
 mp.add_key_binding('Shift+DEL', 'delete-file', function()
-	if not mp.get_property_bool('pause') and mp.get_property_number('time-pos') < 1 then
+	if not mp.get_property_bool('pause') and mp.get_property_number('time-pos', 0) < 1 then
 		return
 	end
 
