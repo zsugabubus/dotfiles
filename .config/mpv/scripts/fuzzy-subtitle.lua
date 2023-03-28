af2fecadcd1743b886ef580d2fd8471e41c1ae88
zsugabubus <zsugabubus@users.noreply.github.com>
2023-03-29 01:42:45 +0200

    mpv/fuzzy-subtitle: Improve
    - Fix: Missing `utils.join_path(path, file)` when opening subtitle.
    - Refactor: Use '^' anchor in patterns.
    - Refactor: Change location of temporary file.
    - Refactor: Add better logging.
    - Refactor: Improve code style.

--- .config/mpv/scripts/fuzzy-subtitle.lua
+++ .config/mpv/scripts/fuzzy-subtitle.lua
@@ -1,75 +1,100 @@
+local SUBTITLE_EXTENSIONS = {
+	'srt',
+	'lrc',
+	'txt',
+}
+
 local utils = require('mp.utils')
-local tmp_file = (os.getenv('TMPDIR') or '/tmp') .. '/' .. mp.get_script_name()
+local xdg_cache_dir = (
+	os.getenv('XDG_CACHE_DIR') or
+	utils.join_path(os.getenv('HOME'), '.cache')
+)
+local tmp_prefix = utils.join_path(
+	xdg_cache_dir,
+	'mpv-' .. mp.get_script_name()
+)
 
-mp.register_event('start-file', function()
-	local filename = mp.get_property('path')
-	if filename == nil then
-		return
+mp.msg.debug('Temporary file prefix', tmp_prefix)
+
+local function str2pattern(s)
+	return string.gsub(s, '([^%w])', '%%%1')
+end
+
+local function add_subtitle(file)
+	mp.msg.info('Adding subtitle', file)
+
+	local tmp_file
+	if string.find(file, '%.txt$') then
+		mp.msg.debug('Convert TXT to LRC')
+
+		tmp_file = tmp_prefix .. '.lrc'
+		mp.msg.debug('Using temporary file', tmp_file)
+
+		local f = io.open(file, 'r')
+		local tmp = io.open(tmp_file, 'w')
+		tmp:write('[0:0.0] ')
+		tmp:write(f:read('*all'))
+		tmp:close()
+		f:close()
 	end
-	local dirname, basename = utils.split_path(filename)
 
-	local pats = {}
+	mp.commandv('sub-add', tmp_file or file)
 
-	local basename_pat = text2pat(basename)
-	addpat(pats, basename_pat)
-	addpat(pats, string.match(basename_pat, '(.*)%%%..+'))
-
-	local serno, epno = string.match(basename, '[sS]0*(%d+)[eE]0*(%d+)')
-	if serno ~= nil and epno ~= nil then
-		addpat(pats, '[sS]0*' .. serno .. '[eE]0*' .. epno .. '.*')
-	end
-
-	for _, subdir in ipairs(mp.get_property_native('sub-file-paths')) do
-		walk(dirname .. '/' .. subdir, pats)
-	end
-end)
-
-function addpat(pats, pat)
-	if pat == nil then
-		return
-	end
-
-	for _, ext in pairs({'srt', 'lrc', 'txt'}) do
-		table.insert(pats, pat .. '%.' .. ext .. '$')
+	if tmp_file then
+		os.remove(tmp_file)
 	end
 end
 
-function text2pat(text)
-	return text:gsub('([^%w])', '%%%1')
-end
+local function search(path, patterns)
+	mp.msg.debug('Searching in', path)
 
-function walk(path, pats)
-	local nfiles = 0
+	local num_files = 0
 	for _, file in ipairs(utils.readdir(path, 'files') or {}) do
-		nfiles = nfiles + 1
-
-		for _, pattern in ipairs(pats) do
-
-			if file:find(pattern) ~= nil then
-				local gen = file:find('%.txt$') ~= nil
-				if gen then
-					mp.msg.info('Generating subtitle from', file)
-					local f = io.open(file, 'r')
-					file = tmp_file .. '.lrc'
-					local t = io.open(file, 'w')
-					t:write('[0:0.0] ')
-					t:write(f:read('*all'))
-					f:close()
-					t:close()
-				end
-
-				mp.msg.info('Adding subtitle from', file)
-				mp.commandv('sub-add', file)
-
-				if gen then
-					os.remove(file)
-				end
+		num_files = num_files + 1
+		for _, pattern in ipairs(patterns) do
+			if string.find(file, pattern) then
+				add_subtitle(utils.join_path(path, file))
 			end
 		end
 	end
-	if nfiles <= 1 then
+
+	if num_files <= 1 then
 		for _, dir in ipairs(utils.readdir(path, 'dirs') or {}) do
-			walk(path .. '/' .. dir, pats)
+			search(utils.join_path(path, dir), patterns)
 		end
 	end
 end
+
+mp.register_event('start-file', function()
+	local path = mp.get_property('path')
+	if not path then
+		return
+	end
+	local base_dir, filename = utils.split_path(path)
+
+	local patterns = {}
+
+	local function register(pattern)
+		for _, ext in pairs(SUBTITLE_EXTENSIONS) do
+			full_pattern = string.format('^%s%%.%s$', pattern, ext)
+			mp.msg.debug('Use pattern', full_pattern)
+			table.insert(patterns, full_pattern)
+		end
+	end
+
+	register(str2pattern(filename))
+
+	local without_ext = string.match(filename, '^(.+)%..+$')
+	if without_ext then
+		register(str2pattern(without_ext))
+	end
+
+	local season, episode = string.match(filename, '[sS]0*(%d+)[eE]0*(%d+)')
+	if season and episode then
+		register(string.format('.*[sS]0*%s[eE]0*%s.*', season, episode))
+	end
+
+	for _, subdir in ipairs(mp.get_property_native('sub-file-paths')) do
+		search(utils.join_path(base_dir, subdir), patterns)
+	end
+end)
