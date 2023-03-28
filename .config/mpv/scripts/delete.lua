45e4c252e273e0a53e3563fe538af5654e912ba5
zsugabubus <zsugabubus@users.noreply.github.com>
2023-03-29 00:26:24 +0200

    mpv/delete: Fix race condition

--- .config/mpv/scripts/delete.lua
+++ .config/mpv/scripts/delete.lua
@@ -1,31 +1,30 @@
 local utils = require('mp.utils')
+
 mp.add_key_binding('Shift+DEL', 'delete-file', function()
-	if not mp.get_property_bool('pause') and mp.get_property_number('time-pos', 0) < 1 then
+	local path = mp.get_property_native('path')
+	if not path then
 		return
 	end
 
-	local work_dir = mp.get_property_native('working-directory')
-	local file_path = mp.get_property_native('path')
-	local s = file_path:find(work_dir, 0, true)
-	local path
-	if s and s == 0 then
-		path = file_path
-	else
-		path = utils.join_path(work_dir, file_path)
+	-- Perform safety checks after 'path' has been get to avoid race conditions.
+	if (
+		not mp.get_property_bool('pause') and
+		mp.get_property_number('time-pos', 0) < 1
+	) then
+		mp.msg.error('Safety check blocked deleting file that has just started to play')
+		mp.msg.info('To delete the file pause playback or try again a few moments later')
+		mp.osd_message('Delete blocked')
+		return
 	end
 
-	-- Is file?
-	local f = io.open(path, 'r')
-	if f ~= nil then
-		io.close(f)
-		local dir_name, _ = utils.split_path(path)
-		if os.rename(path, dir_name .. '/.deleted') then
-			mp.command('playlist_remove current')
-			local msg = ('Deleted: %s'):format(file_path)
-			mp.msg.info(msg)
-			mp.osd_message(msg)
-		else
-			mp.osd_message('Failed to delete')
-		end
+	local dirname, _ = utils.split_path(path)
+	local new_path = utils.join_path(dirname, '.deleted')
+
+	if os.rename(path, new_path) then
+		mp.msg.info(('Moved: %s -> %s'):format(path, new_path))
+		mp.osd_message('File deleted')
+		mp.command('playlist_remove current')
+	else
+		mp.osd_message('Delete failed')
 	end
 end, {repeatable = false})
