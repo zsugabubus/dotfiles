1385a63ff96579ce15020c15ec49460ab605797f
zsugabubus <zsugabubus@users.noreply.github.com>
2023-03-29 21:48:20 +0200

    mpv/mouse: Refactor

--- .config/mpv/scripts/mouse.lua
+++ .config/mpv/scripts/mouse.lua
@@ -9,40 +9,50 @@ local function get_tile()
 		math.floor(pos.x * 3 / w)
 end
 
-local function scroll(e)
-	local scroll = e.key_name:find('WHEEL_') ~= nil
-
+local function handle(e)
 	-- "down" on X, "press" on Wayland.
 	if e.event == 'up' then
 		return
 	end
 
 	local tile = get_tile()
-	local up = e.key_name:find('UP')
-	local small = not e.key_name:find('Shift+')
+	local up = string.find(e.key_name, 'UP')
+	local small = not string.find(e.key_name, 'Shift+')
+	local scroll = string.find(e.key_name, 'WHEEL_') ~= nil
+
 	if tile == 0 or tile == 3 then
 		if scroll then
 			mp.commandv(up and 'frame-step' or 'frame-back-step')
 		end
 	elseif tile == 1 then
 		if scroll then
-			mp.commandv('script-message', up and 'playlist-next' or 'playlist-prev')
+			mp.commandv(
+				'script-message',
+				up and 'playlist-next' or 'playlist-prev'
+			)
 		end
 		mp.commandv('script-message-to', 'osd_playlist', 'peek')
 	elseif tile == 2 or tile == 5 then
 		if scroll then
-			mp.commandv('osd-msg-bar', 'add', 'volume', (up and 1 or -2) * (small and 1 or 3))
+			mp.commandv(
+				'osd-msg-bar',
+				'add',
+				'volume',
+				(up and 1 or -2) * (small and 1 or 3)
+			)
 		else
 			mp.commandv('osd-msg-bar', 'set', 'volume', 100)
 		end
-	elseif scroll and tile == 6 then
-		mp.commandv(
-			'script-message-to',
-			'osd_bar',
-			'seek',
-			(up and 1 or -2) * (small and 1 or 3),
-			'exact'
-		)
+	elseif tile == 6 then
+		if scroll then
+			mp.commandv(
+				'script-message-to',
+				'osd_bar',
+				'seek',
+				(up and 1 or -2) * (small and 1 or 3),
+				'exact'
+			)
+		end
 	else
 		if scroll then
 			mp.commandv(
@@ -58,6 +68,6 @@ end
 
 for _, mod in ipairs({'', 'Shift+'}) do
 	for _, key in ipairs({'WHEEL_UP', 'WHEEL_DOWN', 'MBTN_MID'}) do
-		mp.add_forced_key_binding(mod .. key, scroll, {complex = true})
+		mp.add_forced_key_binding(mod .. key, handle, {complex = true})
 	end
 end
