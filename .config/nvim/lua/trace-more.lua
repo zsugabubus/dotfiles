b3e36d05aa58816f5e876ddfca4fde34164975d4
zsugabubus <zsugabubus@users.noreply.github.com>
2023-05-02 13:25:38 +0200

    nvim: Use string.buffer to produce trace report

--- .config/nvim/lua/trace-more.lua
+++ .config/nvim/lua/trace-more.lua
@@ -1,10 +1,10 @@
 local M = {}
 
 function M.report(node)
-	local lines = {
-		string.format('%11s %11s %11s %s', 'clock', 'total', 'self', 'event'),
-	}
 	local epoch = node.start
+
+	local buf = require 'string.buffer'.new()
+	buf:putf('%11s %11s %11s %s\n', 'clock', 'total', 'self', 'event')
 
 	local function walk(node, depth)
 		local children_elapsed = 0
@@ -12,14 +12,14 @@ function M.report(node)
 			children_elapsed = children_elapsed + child.elapsed
 		end
 
-		table.insert(lines, string.format(
-			'%8.3f ms %8.3f ms %8.3f ms %s%s',
+		buf:putf(
+			'%8.3f ms %8.3f ms %8.3f ms %s%s\n',
 			(node.start - epoch) / 1e6,
 			node.elapsed / 1e6,
 			(node.elapsed - children_elapsed) / 1e6,
 			string.rep(' ', depth * 2),
 			node.name
-		))
+		)
 
 		for _, child in ipairs(node) do
 			walk(child, depth + 1)
@@ -28,7 +28,7 @@ function M.report(node)
 
 	walk(node, 0)
 
-	return table.concat(lines, '\n')
+	return buf:tostring()
 end
 
 function M.startuptime(verbose)
