715f82ada60df05d5451f35b15cd4e8f72704b9c
zsugabubus <zsugabubus@users.noreply.github.com>
2023-05-03 01:12:58 +0200

    mpv/playlist-random: Simplify random seeder

--- .config/mpv/scripts/playlist-random.lua
+++ .config/mpv/scripts/playlist-random.lua
@@ -1,14 +1,14 @@
-(function()
+do
 	local f = assert(io.open('/dev/random', 'rb'))
 
-	local seed, s = 0, f:read(4)
-	for i = 1, s:len() do
-		seed = 256 * seed + s:byte(i)
+	local seed = 0
+	for i = 1, 4 do
+		seed = 256 * seed + string.byte(f:read(1), 1)
 	end
 	math.randomseed(seed)
 
-	io.close(f)
-end)()
+	f:close()
+end
 
 mp.add_key_binding('r', 'playlist-random', function()
 	local pos = math.random(0, mp.get_property_number('playlist-count') - 1)
