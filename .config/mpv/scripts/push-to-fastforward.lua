c0c8adb54e569df036c42529ad2bde643adb1099
zsugabubus <zsugabubus@users.noreply.github.com>
2023-06-02 21:35:36 +0200

    mpv/push-to-fastforward: Handle 'press' event

---
+++ .config/mpv/scripts/push-to-fastforward.lua
@@ -0,0 +1,27 @@
+local options = require 'mp.options'
+
+local opts = {
+	speed = 2.71828 -- speeeeeeed
+}
+options.read_options(opts)
+
+local down = true
+
+-- script-binding push-to-fastforward
+mp.add_key_binding(nil, 'push-to-fastforward', function(event)
+	if event.event == 'down' then
+		down = true
+	elseif event.event == 'up' then
+		down = false
+	elseif event.event == 'press' then
+		down = not down
+	end
+
+	if down then
+		mp.set_property_number('speed', opts.speed)
+		mp.osd_message(string.format('▶▶ x%.2f', opts.speed))
+	else
+		mp.set_property_number('speed', 1)
+		mp.osd_message('')
+	end
+end, {complex = true})
