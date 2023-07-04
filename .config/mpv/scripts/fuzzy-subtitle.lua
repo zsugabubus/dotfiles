388d75e13c706ca27a8eadd398d646889c072183
zsugabubus <zsugabubus@users.noreply.github.com>
2023-07-04 21:19:17 +0200

    mpv/fuzzy-subtitle: Add "SxE" pattern

--- .config/mpv/scripts/fuzzy-subtitle.lua
+++ .config/mpv/scripts/fuzzy-subtitle.lua
@@ -13,21 +13,14 @@ local XDG_CACHE_DIR = (
 local TMP_PREFIX =
 	utils.join_path(XDG_CACHE_DIR, 'mpv-' .. mp.get_script_name())
 
-mp.msg.debug('Temporary file prefix', TMP_PREFIX)
-
-local function str2pattern(s)
+local function pesc(s)
 	return string.gsub(s, '([^%w])', '%%%1')
 end
 
 local function add_subtitle(file)
-	mp.msg.info('Adding subtitle', file)
-
 	local tmp_file
 	if string.find(file, '%.txt$') then
-		mp.msg.debug('Convert TXT to LRC')
-
 		tmp_file = TMP_PREFIX .. '.lrc'
-		mp.msg.debug('Using temporary file', tmp_file)
 
 		local f = io.open(file, 'r')
 		local tmp = io.open(tmp_file, 'w')
@@ -44,9 +37,36 @@ local function add_subtitle(file)
 	end
 end
 
-local function search(path, patterns)
-	mp.msg.debug('Searching in', path)
+local function patterns_from_filename(filename)
+	local patterns = {}
 
+	local function add_pattern(pattern)
+		for _, ext in pairs(SUBTITLE_EXTENSIONS) do
+			local full_pattern = string.format('^%s%%.%s$', pattern, ext)
+			table.insert(patterns, full_pattern)
+		end
+	end
+
+	add_pattern(pesc(filename))
+
+	local without_ext = string.match(filename, '^(.+)%..+$')
+	if without_ext then
+		add_pattern(pesc(without_ext))
+	end
+
+	local season, episode = string.match(filename, '[sS]0*(%d+)[eE]0*(%d+)')
+	if not season then
+		season, episode = string.match(filename, '0*(%d+)[xX]0*(%d+)')
+	end
+	if season then
+		add_pattern(string.format('.*[sS]0*%s[eE]0*%s.*', season, episode))
+		add_pattern(string.format('.*%%D%s[xX]0*%s%%D.*', season, episode))
+	end
+
+	return patterns
+end
+
+local function search(path, patterns)
 	local num_files = 0
 	for _, file in ipairs(utils.readdir(path, 'files') or {}) do
 		num_files = num_files + 1
@@ -77,28 +97,7 @@ mp.register_event('start-file', function()
 	end
 
 	local dirname, filename = utils.split_path(path)
-
-	local patterns = {}
-
-	local function register(pattern)
-		for _, ext in pairs(SUBTITLE_EXTENSIONS) do
-			full_pattern = string.format('^%s%%.%s$', pattern, ext)
-			mp.msg.debug('Use pattern', full_pattern)
-			table.insert(patterns, full_pattern)
-		end
-	end
-
-	register(str2pattern(filename))
-
-	local without_ext = string.match(filename, '^(.+)%..+$')
-	if without_ext then
-		register(str2pattern(without_ext))
-	end
-
-	local season, episode = string.match(filename, '[sS]0*(%d+)[eE]0*(%d+)')
-	if season and episode then
-		register(string.format('.*[sS]0*%s[eE]0*%s.*', season, episode))
-	end
+	local patterns = patterns_from_filename(filename)
 
 	for _, subdir in ipairs(mp.get_property_native('sub-file-paths')) do
 		search(utils.join_path(dirname, subdir), patterns)
