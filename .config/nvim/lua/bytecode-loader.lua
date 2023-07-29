d8c8c1fa82270e3776227d3db37efbff08caed3f
zsugabubus <zsugabubus@users.noreply.github.com>
2023-07-29 23:07:19 +0200

    nvim/bytecode: Ensure cache directory exists

--- .config/nvim/lua/bytecode-loader.lua
+++ .config/nvim/lua/bytecode-loader.lua
@@ -1,5 +1,6 @@
 local Trace = require('trace')
-local cache_dir = vim.fn.stdpath('cache') .. '/bytecode'
+local cache_dir = vim.fn.stdpath('cache')
+local bytecode_dir = cache_dir .. '/bytecode'
 local loadfile = loadfile -- Make it local.
 
 function _G.loadfile(path)
@@ -15,7 +16,7 @@ function _G.loadfile(path)
 	local path_key = string.gsub(path, '/', '%%')
 	local cache_path = string.format(
 		'%s/%s%%M=%s_%s,S=%s',
-		cache_dir,
+		bytecode_dir,
 		path_key,
 		stat.mtime.sec,
 		stat.mtime.nsec,
@@ -48,69 +49,74 @@ function _G.loadfile(path)
 	end
 
 	uv.fs_mkdir(cache_dir, tonumber('700', 8), function()
-		local tmp_path = string.format('%s.%d~', cache_path, uv.os_getpid())
-		uv.fs_open(tmp_path, 'wx', tonumber('600', 8), function(err, fd)
-			-- Maybe another loadfile() would like to cache concurrently, or maybe
-			-- something below failed so do not try again.
-			if err and string.find(err, '^EEXIST:') then
-				return
-			end
-			-- Cannot do much about it.
-			if err and string.find(err, '^EROFS:') then
-				return
-			end
-			assert(not err, err)
-			-- LuaJIT accepts a second argument "strip". When set, the produced
-			-- bytecode will be free from debug information that results in smaller
-			-- size and faster loading.
-			--
-			-- Important: The so produced bytecode is not that much portable so
-			-- version of Nvim (that hopefully identifies version of LuaJIT) must
-			-- always be present in cache key.
-			--
-			-- See: https://luajit.org/extensions.html.
-			uv.fs_write(fd, string.dump(code, true), -1, function(err)
+		uv.fs_mkdir(bytecode_dir, tonumber('700', 8), function()
+			local tmp_path = string.format('%s.%d~', cache_path, uv.os_getpid())
+			uv.fs_open(tmp_path, 'wx', tonumber('600', 8), function(err, fd)
+				-- Maybe another loadfile() would like to cache concurrently, or maybe
+				-- something below failed so do not try again.
+				if err and string.find(err, '^EEXIST:') then
+					return
+				end
+				-- Cannot do much about it.
+				if err and string.find(err, '^EROFS:') then
+					return
+				end
 				assert(not err, err)
-				uv.fs_close(fd, function(err, success)
+				-- LuaJIT accepts a second argument "strip". When set, the produced
+				-- bytecode will be free from debug information that results in smaller
+				-- size and faster loading.
+				--
+				-- Important: The so produced bytecode is not that much portable so
+				-- version of Nvim (that hopefully identifies version of LuaJIT) must
+				-- always be present in cache key.
+				--
+				-- See: https://luajit.org/extensions.html.
+				uv.fs_write(fd, string.dump(code, true), -1, function(err)
 					assert(not err, err)
-					assert(success)
-
-					uv.fs_opendir(cache_dir, function(err, dir)
+					uv.fs_close(fd, function(err, success)
 						assert(not err, err)
+						assert(success)
 
-						local function read_next(err, entries)
+						uv.fs_opendir(bytecode_dir, function(err, dir)
 							assert(not err, err)
-							if not entries then
-								uv.fs_closedir(dir, function(err, success)
-									assert(not err, err)
-									assert(success)
-								end)
 
-								uv.fs_rename(tmp_path, cache_path, function(err, success)
-									assert(not err, err)
-									assert(success)
-								end)
-								return
-							end
-
-							-- Clean up old entries.
-							for _, entry in ipairs(entries) do
-								local name = string.match(entry.name, '(.*)%%.*[^~]$')
-								if name == path_key then
-									uv.fs_unlink(cache_dir .. '/' .. entry.name, function(err)
-										if err and string.find(err, '^ENOENT:') then
-											return
-										end
+							local function read_next(err, entries)
+								assert(not err, err)
+								if not entries then
+									uv.fs_closedir(dir, function(err, success)
 										assert(not err, err)
 										assert(success)
 									end)
+
+									uv.fs_rename(tmp_path, cache_path, function(err, success)
+										assert(not err, err)
+										assert(success)
+									end)
+									return
 								end
+
+								-- Clean up old entries.
+								for _, entry in ipairs(entries) do
+									local name = string.match(entry.name, '(.*)%%.*[^~]$')
+									if name == path_key then
+										uv.fs_unlink(
+											bytecode_dir .. '/' .. entry.name,
+											function(err)
+												if err and string.find(err, '^ENOENT:') then
+													return
+												end
+												assert(not err, err)
+												assert(success)
+											end
+										)
+									end
+								end
+
+								uv.fs_readdir(dir, read_next)
 							end
 
 							uv.fs_readdir(dir, read_next)
-						end
-
-						uv.fs_readdir(dir, read_next)
+						end)
 					end)
 				end)
 			end)
