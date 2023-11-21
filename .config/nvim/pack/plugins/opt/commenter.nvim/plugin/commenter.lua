a0eb57eef7739712cf08b35870f0e3f9e406ebc4
zsugabubus <zsugabubus@users.noreply.github.com>
2023-11-21 01:12:46 +0100

    nvim/commenter: Refactor

--- .config/nvim/pack/plugins/opt/commenter.nvim/plugin/commenter.lua
+++ .config/nvim/pack/plugins/opt/commenter.nvim/plugin/commenter.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_set_keymap('n', '<Plug>(commenter)', '', {
 	expr = true,
 	callback = function()
-		vim.o.opfunc = 'v:lua._commenter_opfunc'
+		vim.o.operatorfunc = 'v:lua._commenter_operatorfunc'
 		return 'g@'
 	end,
 })
@@ -11,14 +11,14 @@ vim.api.nvim_set_keymap('n', '<Plug>(commenter-current-line)', '', {
 	replace_keycodes = true,
 	callback = function()
 		if vim.v.count == 0 then
-			return '<Plug>(commenter)V0'
+			return '<Plug>(commenter)_'
 		else
-			return '<Plug>(commenter)Vj'
+			return '<Plug>(commenter)j'
 		end
 	end,
 })
 
-function _G._commenter_opfunc()
+function _G._commenter_operatorfunc()
 	require('commenter').comment_lines(
 		vim.api.nvim_buf_get_mark(0, '[')[1],
 		vim.api.nvim_buf_get_mark(0, ']')[1]
