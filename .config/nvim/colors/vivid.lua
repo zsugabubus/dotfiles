63cceed50bd1baf12ec0fba5129cf98ebc60bec5
zsugabubus <zsugabubus@users.noreply.github.com>
2023-12-03 19:40:18 +0100

    nvim/vivid: Update colors

--- .config/nvim/colors/vivid.lua
+++ .config/nvim/colors/vivid.lua
@@ -11,7 +11,8 @@ if vim.v.vim_did_enter ~= 0 then
 	end
 end
 
-local light = vim.o.background == 'light'
+local light = vim.o.background == 'light' or nil
+local dark = not light or nil
 
 local span = trace('colorscheme data')
 
@@ -20,24 +21,23 @@ local theme = {
 	Conceal = 'Normal',
 	Directory = 'Normal',
 	EndOfBuffer = 'NonText',
-	Folded = { bg = '#e1e1e1', fg = '#666666' },
+	Folded = { bg = light and '#e3e5ea' or '#44455c', fg = light and '#44546a' or '#d1d2d6' },
 	NonText = { fg = light and '#d1d0d3' or '#484848' },
 	Normal = { fg = light and '#2f323f' or '#eeede9' },
-	Search = { fg = '#040404', bg = '#fdef39' },
+	Search = { fg = '#000000', bg = '#fdef39' },
 	Special = 'Normal',
 	SpecialKey = 'NonText',
 	VertSplit = { fg = '#ababab' },
-	Visual = { bg = light and '#acd0fe' or '#4c4dbd', ctermbg = 4 },
-	VisualNOS = { fg = '#d8d8dc' },
+	Visual = { bg = light and '#a0d0ff' or '#4c5dfd', fg = dark and '#ffffff', ctermbg = 4 },
 
-	TabLine = { fg = light and '#444444' or '#d8ccd8', bg = 'StatusLine' },
+	TabLine = { fg = 'StatusLineNC', bg = 'StatusLine' },
 	TabLineFill = 'TabLine',
-	TabLineSel = { fg = light and '#414140' or 'Normal', bg = light and '#eeeeee' or 'Normal', bold = true },
+	TabLineSel = { fg = 'StatusLine', bg = light and '#eeeeee' or 'Normal', bold = true },
 
-	StatusLine = { fg = light and '#313140' or '#f8ece8', bg = light and '#e8e8e8' or '#363a46', bold = true },
-	StatusLineModeTerm = { fg = '#ffffff', bg = '#9D3695', bold = true },
+	StatusLine = { fg = light and '#313140' or '#f8ece8', bg = light and '#e8e8e8' or '#363a4a', bold = true },
+	StatusLineModeTerm = { fg = '#ffffff', bg = '#9d3695', bold = true },
 	StatusLineModeTermEnd = { fg = 'StatusLineModeTerm.bg', bg = 'StatusLine' },
-	StatusLineNC = { fg = light and '#404040' or '#d8ccd8', bg = 'StatusLine' },
+	StatusLineNC = { fg = light and '#404040' or '#d0ccd8', bg = 'StatusLine' },
 	StatusLineTerm = 'Normal',
 	StatusLineTermNC = 'StatusLineTerm',
 
@@ -47,40 +47,36 @@ local theme = {
 	User9 = { fg = light and '#888888' or '#a4a4a4', bg = 'StatusLine' },
 
 	Error = { fg = '#d80000', bg = '#ffd4d4', bold = true },
-	ErrorMsg = { fg = '#fd1d54', bg = '#fdcdd4', bold = true },
+	ErrorMsg = { fg = '#ff0034', bg = '#fdcdd4', bold = true },
 	MoreMsg = { fg = '#1c891a', bold = true },
 	Question = { fg = '#1ca53c', bold = true },
 	WarningMsg = { fg = '#933ab7', bg = '#e0d7f7', bold = true },
 
-	CursorLineNr = { fg = 'Number', bg = 'LineNr', bold = true },
+	CursorLineNr = { fg = 'Number', bg = 'LineNr', bold = true, ctermfg = 1 },
 	FoldColumn = { fg = '#b0abab', bg = 'LineNr' },
-	LineNr = { fg = light and '#b0abab' or '#8085a0', bg = light and '#ededed' or nil },
+	LineNr = { fg = light and '#b0abab' or '#8085a0', bg = light and '#ededed' },
 	SignColumn = { fg = '#444444', bg = 'LineNr' },
 
 	Character = 'String',
-	Comment = { fg = '#838385' },
-	Conditional = { fg = light and '#9d00c5' or '#ff6de9', bold = true },
-	Constant = { fg = '#df4f00' },
+	Comment = { fg = '#808082' },
+	Conditional = { fg = light and '#9d00c5' or '#ff6de9', bold = true, ctermfg = 5 },
+	Constant = { fg = '#df4f00', ctermfg = 3 },
 	Identifier = { fg = light and '#4d4d4f' or '#cacacc', bold = true },
-	Keyword = { fg = light and '#006de9' or '#009df9', bold = true },
-	Number = { fg = light and '#fa3422' or '#fa7452', bold = true },
-	PreProc = { fg = light and '#006dd9' or '#ffcd09' },
+	Keyword = { fg = light and '#006de9' or '#009df9', bold = true, ctermfg = 4 },
+	Number = { fg = light and '#fa3422' or '#fa7452', bold = true, ctermfg = 1 },
+	PreProc = { fg = 'Keyword', ctermfg = 4 },
 	Repeat = 'Conditional',
 	SpecialChar = { fg = '#df4f00', bold = true },
 	Statement = 'Keyword',
 	String = 'Constant',
-	Type = { fg = light and '#ed0085' or '#fd0069', bold = true },
+	Type = { fg = light and '#ed0085' or '#fd0069', bold = true, ctermfg = 1 },
 
-	DiffAdd = { bg = light and '#b5f78b' or nil, fg = not light and '#2edd2e' or nil },
-	DiffChange = { bg = light and '#ffdda5' or nil, fg = not light and '#ffc028' or nil },
+	DiffAdd = { bg = light and '#b5f789', fg = dark and '#00d700' },
+	DiffChange = { bg = light and '#ffdda5', fg = dark and '#ffc028' },
 	DiffDelete = { bg = light and '#ffa195' or '#ea1f24', fg = light and '#9d1e17' or '#fff0f0' },
-	diffAdded = { fg = light and '#00a206' or '#2edd2e' },
-	diffRemoved = { fg = '#fe2e1f' },
-	diffText = { bg = light and '#ffafff' or '#ffc028', fg = not light and '#000000' or nil },
-
-	GitSignAdd = { bg = light and '#b5f78b' or '#2edd2e', fg = '#000000', bold = true, },
-	GitSignDelete = { bg = light and '#ffa195' or '#ea1f24', fg = light and '#000000' or '#ffffff', bold = true, },
-	GitSignChange = { bg = light and '#ffdda5' or '#ffc028', fg = '#000000', bold = true, },
+	diffAdded = { fg = light and '#00af00' or '#00d700' },
+	diffRemoved = { fg = '#ff0000' },
+	diffText = { bg = light and '#ffafff' or '#ffc028', fg = dark and '#000000' },
 
 	Pmenu = { bg = '#e0e0e0', fg = '#303030' },
 	PmenuSbar = { bg = 'Pmenu' },
@@ -88,20 +84,18 @@ local theme = {
 	PmenuThumb = { bg = '#adadad' },
 	WildMenu = { bg = 'Visual', fg = 'Visual', bold = true },
 
-	SpellBad = { bg = '#f9f2f4', fg = '#c72750', undercurl = true, sp = '#d73750' },
+	SpellBad = { undercurl = true },
 	Tag = {},
 	Title = { fg = '#858585' },
-	Todo = { fg = '#1ca53c', bold = true, italic = true },
+	Todo = { fg = '#1ca53c', bold = true, italic = true, ctermfg = 2 },
 	Underlined = { fg = '#008df9' },
-
-	MatchParen = { bg = '#fde639', fg = '#111111', bold = true },
 
 	CfgOnOff = 'Boolean',
 
 	Boolean = 'cConstant',
 	Label = 'cUserLabel',
 	Structure = 'Keyword',
-	cConstant = { fg = light and '#009017' or '#1ca51c', bold = true },
+	cConstant = { fg = light and '#009017' or '#1ca51c', bold = true, ctermfg = 2 },
 	cLabel = 'Keyword',
 	cOctalZero = { fg = '#8700af', bold = true },
 	cStorageClass = 'Keyword',
@@ -157,7 +151,6 @@ local theme = {
 	vimOper = 'Normal',
 	vimOperParen = 'Normal',
 	vimOption = 'Identifier',
-	vimStatement = { fg = '#005db2' },
 	vimVar = 'Normal',
 
 	javaScriptBraces = 'Normal',
@@ -177,9 +170,6 @@ local theme = {
 	phpConstant = 'cConstant',
 
 	gitReference = 'diffFile',
-
-	debugPC = { bg = '#fff500' },
-	debugBreakpoint = { bg = '#f51030', fg = '#ffffff' },
 
 	changeLogError = {},
 
