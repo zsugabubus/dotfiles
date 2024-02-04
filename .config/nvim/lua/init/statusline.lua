57643b9056786e6474912f23478766b2c600a4a1
zsugabubus <zsugabubus@users.noreply.github.com>
2024-02-04 16:16:02 +0100

    nvim: Fix code formatting

--- .config/nvim/lua/init/statusline.lua
+++ .config/nvim/lua/init/statusline.lua
@@ -25,23 +25,20 @@ vim.api.nvim_create_autocmd('WinLeave', {
 	end,
 })
 
-vim.api.nvim_create_autocmd(
-	{ 'VimEnter', 'WinEnter', 'BufWinEnter' },
-	{
-		group = group,
-		callback = function()
-			vim.wo.statusline = table.concat({
-				"%(%#StatusLineModeTerm#%{'t'==mode()?'  T ':''}%#StatusLineModeTermEnd#%{'t'==mode()?' ':''}%#StatusLine#%)",
-				"%(%( %{!&diff&&argc()>#1?(argidx()+1).' of '.argc():''} %)%(  %{v:lua.git_status()} %) %)",
-				'%n:%f%( %h%w%r%)%( %m%)',
-				'%9*%#StatusLine#',
-				'%= ',
-				'%1*%2*',
-				"%(  %{&spell?&spelllang:''} %)",
-				"%(  %{substitute((empty(&fileencoding)?'utf-8':&fileencoding).(&bomb?',bom':'').','.&fileformat,'^utf-8,unix$','','')} %)",
-				"%( %{&filetype} %)",
-				'%3* %l(%{lnum_status})/%L,%-3v',
-			})
-		end,
-	}
-)
+vim.api.nvim_create_autocmd({ 'VimEnter', 'WinEnter', 'BufWinEnter' }, {
+	group = group,
+	callback = function()
+		vim.wo.statusline = table.concat({
+			"%(%#StatusLineModeTerm#%{'t'==mode()?'  T ':''}%#StatusLineModeTermEnd#%{'t'==mode()?' ':''}%#StatusLine#%)",
+			"%(%( %{!&diff&&argc()>#1?(argidx()+1).' of '.argc():''} %)%(  %{v:lua.git_status()} %) %)",
+			'%n:%f%( %h%w%r%)%( %m%)',
+			'%9*%#StatusLine#',
+			'%= ',
+			'%1*%2*',
+			"%(  %{&spell?&spelllang:''} %)",
+			"%(  %{substitute((empty(&fileencoding)?'utf-8':&fileencoding).(&bomb?',bom':'').','.&fileformat,'^utf-8,unix$','','')} %)",
+			'%( %{&filetype} %)',
+			'%3* %l(%{lnum_status})/%L,%-3v',
+		})
+	end,
+})
