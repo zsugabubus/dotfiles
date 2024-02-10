86482e7193e7f99f1a90be4226fb4c874b5396fb
zsugabubus <zsugabubus@users.noreply.github.com>
2024-02-10 22:51:52 +0100

    nvim/colors: Refactor parser and add more tests

--- .config/nvim/pack/plugins/opt/colors.nvim/rust/packages/nvim-plugin-colors/src/parser.rs
+++ .config/nvim/pack/plugins/opt/colors.nvim/rust/packages/nvim-plugin-colors/src/parser.rs
@@ -5,20 +5,23 @@ use palette::convert::FromColorUnclamped;
 use palette::rgb::channels;
 use palette::{Hsl, Hwb, Lab, Lch, LinSrgb, Oklab, Oklch, Srgb};
 
-macro_rules! color_to_u32 {
-    (
-        $color:expr
-    ) => {
-        Srgb::<u8>::from_linear(LinSrgb::from_color_unclamped($color)).into_u32::<channels::Rgba>()
-            >> 8
+macro_rules! convert_rgb {
+    ($rgb:expr) => {
+        $rgb.into_u32::<channels::Rgba>() >> 8
     };
 }
 
-macro_rules! rgb_to_u32 {
-    (
-        $rgb:expr
-    ) => {
-        $rgb.into_u32::<channels::Rgba>() >> 8
+macro_rules! convert_lossy {
+    ($color:expr) => {
+        convert_rgb!(Srgb::<u8>::from_linear(LinSrgb::from_color_unclamped(
+            $color
+        )))
+    };
+}
+
+macro_rules! convert_lossless {
+    ($color:expr) => {
+        convert_rgb!(Srgb::from_color($color).into_format::<u8>())
     };
 }
 
@@ -108,7 +111,7 @@ fn angle(input: &[u8], pos: usize) -> Result<f32> {
     }
 }
 
-fn css_generic_fn<A, B, C>(
+fn css_fn<A, B, C>(
     input: &[u8],
     pos: usize,
     a: impl FnOnce(&[u8], usize) -> Result<A>,
@@ -130,12 +133,9 @@ pub fn css_rgb_fn(input: &[u8], pos: usize) -> Result<u32> {
         map(number_or_percentage(input, pos, 255.0), |x| x / 255.0)
     }
 
-    let (pos, (r, g, b)) = css_generic_fn(input, pos, component, component, component)?;
-    let color = Srgb::new(r, g, b);
-    Ok((
-        pos,
-        color.into_format::<u8>().into_u32::<channels::Rgba>() >> 8,
-    ))
+    let (pos, (r, g, b)) = css_fn(input, pos, component, component, component)?;
+    let color = Srgb::new(r, g, b).into_format::<u8>();
+    Ok((pos, convert_rgb!(color)))
 }
 
 pub fn css_hsl_fn(input: &[u8], pos: usize) -> Result<u32> {
@@ -143,27 +143,23 @@ pub fn css_hsl_fn(input: &[u8], pos: usize) -> Result<u32> {
         map(percentage(input, pos, 1.0), |x| x.clamp(0.0, 1.0))
     }
 
-    let (pos, (h, s, l)) = css_generic_fn(input, pos, angle, coord, coord)?;
+    let (pos, (h, s, l)) = css_fn(input, pos, angle, coord, coord)?;
     let color = Hsl::new(h, s, l);
-    let rgb = Srgb::from_color(color).into_format::<u8>();
-    Ok((pos, rgb_to_u32!(rgb)))
+    Ok((pos, convert_lossless!(color)))
 }
 
 pub fn css_hwb_fn(input: &[u8], pos: usize) -> Result<u32> {
-    let (pos, (h, w, b)) = css_generic_fn(
-        input,
-        pos,
-        angle,
-        |input, pos| percentage(input, pos, 100.0),
-        |input, pos| percentage(input, pos, 100.0),
-    )?;
+    fn coord(input: &[u8], pos: usize) -> Result<f32> {
+        percentage(input, pos, 100.0)
+    }
+
+    let (pos, (h, w, b)) = css_fn(input, pos, angle, coord, coord)?;
     let color = Hwb::new(h, w, b);
-    let rgb = Srgb::from_color(color).into_format::<u8>();
-    Ok((pos, rgb_to_u32!(rgb)))
+    Ok((pos, convert_lossless!(color)))
 }
 
 pub fn css_lab_fn(input: &[u8], pos: usize) -> Result<u32> {
-    let (pos, (l, a, b)) = css_generic_fn(
+    let (pos, (l, a, b)) = css_fn(
         input,
         pos,
         |input, pos| number_or_percentage(input, pos, 100.0),
@@ -171,11 +167,11 @@ pub fn css_lab_fn(input: &[u8], pos: usize) -> Result<u32> {
         |input, pos| number_or_percentage(input, pos, 125.0),
     )?;
     let color = Lab::new(l, a, b);
-    Ok((pos, color_to_u32!(color)))
+    Ok((pos, convert_lossy!(color)))
 }
 
 pub fn css_lch_fn(input: &[u8], pos: usize) -> Result<u32> {
-    let (pos, (l, c, h)) = css_generic_fn(
+    let (pos, (l, c, h)) = css_fn(
         input,
         pos,
         |input, pos| number_or_percentage(input, pos, 100.0),
@@ -183,11 +179,11 @@ pub fn css_lch_fn(input: &[u8], pos: usize) -> Result<u32> {
         angle,
     )?;
     let color = Lch::new(l, c, h);
-    Ok((pos, color_to_u32!(color)))
+    Ok((pos, convert_lossy!(color)))
 }
 
 pub fn css_oklab_fn(input: &[u8], pos: usize) -> Result<u32> {
-    let (pos, (l, a, b)) = css_generic_fn(
+    let (pos, (l, a, b)) = css_fn(
         input,
         pos,
         |input, pos| number_or_percentage(input, pos, 1.0),
@@ -195,11 +191,11 @@ pub fn css_oklab_fn(input: &[u8], pos: usize) -> Result<u32> {
         |input, pos| number_or_percentage(input, pos, 0.4),
     )?;
     let color = Oklab::new(l, a, b);
-    Ok((pos, color_to_u32!(color)))
+    Ok((pos, convert_lossy!(color)))
 }
 
 pub fn css_oklch_fn(input: &[u8], pos: usize) -> Result<u32> {
-    let (pos, (l, c, h)) = css_generic_fn(
+    let (pos, (l, c, h)) = css_fn(
         input,
         pos,
         |input, pos| number_or_percentage(input, pos, 1.0),
@@ -207,7 +203,7 @@ pub fn css_oklch_fn(input: &[u8], pos: usize) -> Result<u32> {
         angle,
     )?;
     let color = Oklch::new(l, c, h);
-    Ok((pos, color_to_u32!(color)))
+    Ok((pos, convert_lossy!(color)))
 }
 
 #[inline]
@@ -231,8 +227,7 @@ pub unsafe fn css_hex6_unchecked(input: &[u8], pos: usize) -> (usize, u32) {
         component(input, pos + 2),
         component(input, pos + 4),
     );
-
-    (pos + 6, color.into_u32::<channels::Rgba>() >> 8)
+    (pos + 6, convert_rgb!(color))
 }
 
 pub unsafe fn css_hex3_unchecked(input: &[u8], pos: usize) -> (usize, u32) {
@@ -247,17 +242,13 @@ pub unsafe fn css_hex3_unchecked(input: &[u8], pos: usize) -> (usize, u32) {
         component(input, pos + 1),
         component(input, pos + 2),
     );
-
-    (pos + 3, color.into_u32::<channels::Rgba>() >> 8)
+    (pos + 3, convert_rgb!(color))
 }
 
 pub fn xterm256(input: &[u8], pos: usize) -> Result<u32> {
     let (pos, value) = integer(input, pos)?;
     match XTERM_256.get(value as usize) {
-        Some(color) => Ok((
-            pos,
-            Srgb::<u8>::from(*color).into_u32::<channels::Rgba>() >> 8,
-        )),
+        Some(color) => Ok((pos, convert_rgb!(Srgb::<u8>::from(*color)))),
         None => Err(()),
     }
 }
@@ -266,60 +257,173 @@ pub fn xterm256(input: &[u8], pos: usize) -> Result<u32> {
 mod tests {
     use super::*;
 
-    mod rgb {
-        use super::*;
+    #[track_caller]
+    fn parse<T>(f: impl FnOnce(&[u8], usize) -> Result<T>, input: &str, expected: Option<T>)
+    where
+        T: std::fmt::Debug + PartialEq,
+    {
+        let actual = f(input.as_bytes(), 0);
+        let expected = match expected {
+            Some(color) => Ok((input.len(), color)),
+            None => Err(()),
+        };
+        assert_eq!(actual, expected);
+    }
 
-        macro_rules! tests_impl {
-            (
-                $(
-                    $test_name:ident : $input:literal -> Ok($expected_color:expr),
-                )+
-            ) => {
-                $(
-                    #[test]
-                    fn $test_name() {
-                        let actual = css_rgb_fn($input.as_bytes(), 0);
-                        let expected = Ok(($input.len(), $expected_color));
-                        assert_eq!(actual, expected);
-                    }
-                )+
-            };
-            (
-                $(
-                    $test_name:ident : $input:literal -> Err,
-                )+
-            ) => {
-                $(
-                    #[test]
-                    fn $test_name() {
-                        let actual = css_rgb_fn($input.as_bytes(), 0);
-                        let expected = Err(());
-                        assert_eq!(actual, expected);
-                    }
-                )+
-            };
+    #[test]
+    fn parse_space() {
+        parse(space, "", Some(()));
+        parse(space, " ", Some(()));
+        parse(space, "_", Some(()));
+        parse(space, " _ _", Some(()));
+    }
+
+    #[test]
+    fn parse_comma() {
+        parse(comma, "", Some(()));
+        parse(comma, ",", Some(()));
+        parse(comma, ",,", Some(()));
+        parse(comma, " _, ,_ ", Some(()));
+    }
+
+    #[test]
+    fn parse_number() {
+        parse(number, "0", Some(0.0));
+
+        parse(number, "1", Some(1.0));
+        parse(number, ".5", Some(0.5));
+        parse(number, "123", Some(123.0));
+
+        parse(number, "-1", Some(-1.0));
+        parse(number, "-.5", Some(-0.5));
+        parse(number, "-123", Some(-123.0));
+
+        parse(number, "10.01", Some(10.01));
+        parse(number, "10e3", Some(10000.0));
+
+        assert_eq!(number(b"10,", 0), Ok((2, 10.0)));
+
+        parse(number, "", None);
+    }
+
+    #[test]
+    fn parse_integer() {
+        parse(integer, "0", Some(0));
+        parse(integer, "1", Some(1));
+        parse(integer, "12", Some(12));
+        parse(integer, "123", Some(123));
+        parse(integer, "1234", Some(1234));
+
+        parse(integer, "", None);
+        parse(integer, "-1", None);
+        parse(integer, &"9".repeat(100), None);
+    }
+
+    #[test]
+    fn parse_rparen() {
+        parse(rparen, "X(;\n)", Some(()));
+
+        parse(rparen, "", None);
+    }
+
+    #[test]
+    fn parse_number_or_percentage() {
+        assert_eq!(number_or_percentage(b"100", 0, 100.0), Ok((3, 100.0)));
+        assert_eq!(number_or_percentage(b"100%", 0, 100.0), Ok((4, 100.0)));
+        assert_eq!(number_or_percentage(b"100%", 0, 1.0), Ok((4, 1.0)));
+        assert_eq!(number_or_percentage(b"1%", 0, 1000.0), Ok((2, 10.0)));
+        assert_eq!(number_or_percentage(b"0.1%", 0, 1000.0), Ok((4, 1.0)));
+
+        assert_eq!(number_or_percentage(b"", 0, 1.0), Err(()));
+    }
+
+    #[test]
+    fn parse_percentage() {
+        assert_eq!(percentage(b"100%", 0, 100.0), Ok((4, 100.0)));
+        assert_eq!(percentage(b"100%", 0, 1.0), Ok((4, 1.0)));
+        assert_eq!(percentage(b"1%", 0, 1000.0), Ok((2, 10.0)));
+        assert_eq!(percentage(b"0.1%", 0, 1000.0), Ok((4, 1.0)));
+
+        assert_eq!(percentage(b"", 0, 1.0), Err(()));
+    }
+
+    #[test]
+    fn parse_angle_grad() {
+        parse(angle, "400grad", Some(360.0));
+        parse(angle, "800.0GRAD", Some(720.0));
+    }
+
+    #[test]
+    fn parse_angle_rad() {
+        parse(angle, "2rad", Some(2_f32.to_degrees()));
+        parse(angle, ".5RAD", Some(0.5_f32.to_degrees()));
+    }
+
+    #[test]
+    fn parse_angle_turn() {
+        parse(angle, "2turn", Some(720.0));
+        parse(angle, ".5TURN", Some(180.0));
+    }
+
+    #[test]
+    fn parse_angle_deg() {
+        parse(angle, "2deg", Some(2.0));
+        parse(angle, ".5DEG", Some(0.5));
+
+        parse(angle, "DEG", None);
+    }
+
+    #[test]
+    fn parse_angle_unitless() {
+        parse(angle, "1.5", Some(1.5));
+
+        parse(angle, "", None);
+    }
+
+    #[test]
+    fn parse_css_rgb_fn() {
+        parse(css_rgb_fn, "1 2 3)", Some(0x010203));
+        parse(css_rgb_fn, "1 2 3)", Some(0x010203));
+        parse(css_rgb_fn, "1,2,3)", Some(0x010203));
+        parse(css_rgb_fn, "1_2_3)", Some(0x010203));
+        parse(css_rgb_fn, "1 , 2_,_3)", Some(0x010203));
+        parse(css_rgb_fn, " 1 2 3)", Some(0x010203));
+        parse(css_rgb_fn, "_1_2_3)", Some(0x010203));
+        parse(css_rgb_fn, "1 2 3 junk)", Some(0x010203));
+        parse(css_rgb_fn, "1.5 2.5 3.5)", Some(0x020204));
+        parse(css_rgb_fn, "100% 50% 0%)", Some(0xff8000));
+
+        parse(css_rgb_fn, "256 0 0)", Some(0xff0000));
+        parse(css_rgb_fn, "-0.001 0 0)", Some(0x000000));
+
+        parse(css_rgb_fn, "", None);
+        parse(css_rgb_fn, "1 2 junk 3)", None);
+        parse(css_rgb_fn, "1 2)", None);
+        parse(css_rgb_fn, "1 2 3", None);
+    }
+
+    #[test]
+    fn parse_xterm256() {
+        parse(xterm256, "0", Some(0));
+        parse(xterm256, "15", Some(0xffffff));
+        parse(xterm256, "255", Some(0xeeeeee));
+
+        parse(xterm256, "256", None);
+    }
+
+    #[test]
+    fn parse_css_hex6() {
+        unsafe {
+            assert_eq!(css_hex6_unchecked(b"#00aAAa", 1), (7, 0x00aaaa));
+            assert_eq!(css_hex6_unchecked(b"#99FffF", 1), (7, 0x99ffff));
         }
+    }
 
-        tests_impl! {
-            space_separators: "1 2 3)" -> Ok(0x010203),
-            comma_separators: "1,2,3)" -> Ok(0x010203),
-            underscore_separators: "1_2_3)" -> Ok(0x010203),
-            mixed_separators: "1 , 2_,_3)" -> Ok(0x010203),
-            leading_space: " 1 2 3)" -> Ok(0x010203),
-            leading_underscore: "_1_2_3)" -> Ok(0x010203),
-            junk_after_third_component: "1 2 3 junk)" -> Ok(0x010203),
-            floats: "1.5 2.5 3.5)" -> Ok(0x020204),
-            percents: "100% 50% 0%)" -> Ok(0xff8000),
-
-            gamut_overflow: "256 0 0)" -> Ok(0xff0000),
-            gamut_underflow: "-0.001 0 0)" -> Ok(0x000000),
-        }
-
-        tests_impl! {
-            junk_before_third_component: "1 2 junk 3)" -> Err,
-            empty: "" -> Err,
-            missing_component: "1 2)" -> Err,
-            missing_close_parenthesis: "1 2 3" -> Err,
+    #[test]
+    fn parse_css_hex3() {
+        unsafe {
+            assert_eq!(css_hex3_unchecked(b"#0aA", 1), (4, 0x00aaaa));
+            assert_eq!(css_hex3_unchecked(b"#9Ff", 1), (4, 0x99ffff));
         }
     }
 }
