c1479e3472a9ecfc3679a57cad78a928114c8a4a
zsugabubus <zsugabubus@users.noreply.github.com>
2024-03-24 12:43:09 +0100

    nvim/git: Make gitrebase mappings dot-repeatable

--- .config/nvim/pack/plugins/opt/git.nvim/ftplugin/gitrebase.lua
+++ .config/nvim/pack/plugins/opt/git.nvim/ftplugin/gitrebase.lua
@@ -1,21 +1,32 @@
 local keymap = vim.api.nvim_buf_set_keymap
 
-local opts = {
-	nowait = true,
-	silent = true,
-	noremap = true,
-}
-
 vim.b.git_use_preview = true
 
 local function change_command(lhs, command)
-	keymap(
-		0,
-		'',
-		'c' .. lhs,
-		string.format(':normal! 0ce%s<Esc>W', command),
-		opts
-	)
+	keymap(0, '', 'c' .. lhs, '', {
+		nowait = true,
+		silent = true,
+		noremap = true,
+		expr = true,
+		callback = function()
+			function _G._git_rebase_operatorfunc()
+				vim.cmd(string.format("'[,']normal! 0ce%s", command))
+
+				vim.o.operatorfunc = '{->0}'
+
+				vim.cmd.normal({
+					args = { 'g@_W' },
+					bang = true,
+				})
+
+				vim.o.operatorfunc = 'v:lua._git_rebase_operatorfunc'
+			end
+
+			vim.o.operatorfunc = 'v:lua._git_rebase_operatorfunc'
+
+			return 'g@_'
+		end,
+	})
 end
 
 change_command('d', 'drop')
