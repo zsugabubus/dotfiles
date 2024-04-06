95913bbdd2ae2452fc80bc5d1a5893a6d127a078
zsugabubus <zsugabubus@users.noreply.github.com>
2024-04-06 10:46:15 +0200

    nvim/wtf: Remove select mode mappings

--- .config/nvim/pack/plugins/opt/wtf.nvim/plugin/wtf.lua
+++ .config/nvim/pack/plugins/opt/wtf.nvim/plugin/wtf.lua
@@ -1,45 +1,43 @@
 local keymap = vim.api.nvim_set_keymap
 
-keymap('', 'f', '', {
-	expr = true,
-	callback = function()
-		require('wtf').set_search('')
-		return ';'
-	end,
-})
+local empty = {}
 
-keymap('', 'F', '', {
-	expr = true,
-	callback = function()
-		require('wtf').set_search('b')
-		return ';'
-	end,
-})
+local function set_search(lhs, flags)
+	keymap('', lhs, '', {
+		expr = true,
+		replace_keycodes = true,
+		callback = function()
+			require('wtf').set_search(flags)
+			return '<Plug>(wtf-repeat)'
+		end,
+	})
+end
 
-keymap('', 't', '', {
-	expr = true,
-	callback = function()
-		require('wtf').set_search('t')
-		return ';'
-	end,
-})
+set_search('<Plug>(wtf-f)', '')
+set_search('<Plug>(wtf-F)', 'b')
+set_search('<Plug>(wtf-t)', 't')
+set_search('<Plug>(wtf-T)', 'tb')
 
-keymap('', 'T', '', {
-	expr = true,
-	callback = function()
-		require('wtf').set_search('tb')
-		return ';'
-	end,
-})
+local function repeat_search(lhs, flags)
+	keymap('', lhs, '', {
+		callback = function()
+			require('wtf').repeat_search(flags)
+		end,
+	})
+end
 
-keymap('', ';', '', {
-	callback = function()
-		require('wtf').repeat_search('')
-	end,
-})
+repeat_search('<Plug>(wtf-repeat)', '')
+repeat_search('<Plug>(wtf-repeat-opposite)', 'b')
 
-keymap('', ',', '', {
-	callback = function()
-		require('wtf').repeat_search('b')
-	end,
-})
+local function nxo(lhs, rhs)
+	keymap('n', lhs, rhs, empty)
+	keymap('x', lhs, rhs, empty)
+	keymap('o', lhs, rhs, empty)
+end
+
+nxo('f', '<Plug>(wtf-f)')
+nxo('F', '<Plug>(wtf-F)')
+nxo('t', '<Plug>(wtf-t)')
+nxo('T', '<Plug>(wtf-T)')
+nxo(';', '<Plug>(wtf-repeat)')
+nxo(',', '<Plug>(wtf-repeat-opposite)')
