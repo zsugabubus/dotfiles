3dc5c5fd3125d72b073ceb8148800207a9b4e467
zsugabubus <zsugabubus@users.noreply.github.com>
2024-04-06 10:45:46 +0200

    nvim/star: Remove select mode mappings

--- .config/nvim/pack/plugins/opt/star.nvim/plugin/star.lua
+++ .config/nvim/pack/plugins/opt/star.nvim/plugin/star.lua
@@ -1,25 +1,38 @@
 local keymap = vim.api.nvim_set_keymap
 
-keymap('', '*', '', {
+local empty = {}
+
+keymap('', '<Plug>(star-word-forward)', '', {
 	callback = function()
 		require('star').search('w')
 	end,
 })
 
-keymap('', '#', '', {
+keymap('', '<Plug>(star-word-backward)', '', {
 	callback = function()
 		require('star').search('wb')
 	end,
 })
 
-keymap('', 'g*', '', {
+keymap('', '<Plug>(star-forward)', '', {
 	callback = function()
 		require('star').search('')
 	end,
 })
 
-keymap('', 'g#', '', {
+keymap('', '<Plug>(star-backward)', '', {
 	callback = function()
 		require('star').search('b')
 	end,
 })
+
+local function nxo(lhs, rhs)
+	keymap('n', lhs, rhs, empty)
+	keymap('x', lhs, rhs, empty)
+	keymap('o', lhs, rhs, empty)
+end
+
+nxo('*', '<Plug>(star-word-forward)')
+nxo('#', '<Plug>(star-word-backward)')
+nxo('g*', '<Plug>(star-forward)')
+nxo('g#', '<Plug>(star-backward)')
