1bad57fbac59ad52e1b40287681b21a35be2fc0b
zsugabubus <zsugabubus@users.noreply.github.com>
2024-05-29 23:15:28 +0200

    zsh: Add alias "mp.."

--- .config/zsh/30-commands.zsh
+++ .config/zsh/30-commands.zsh
@@ -505,6 +505,7 @@ function mp() {
 }
 compdef mp=mpv_hack
 alias mp.='mp -- *(.)'
+alias mp..='mp -- ***(.)'
 alias mpm='() { eval mp "*(m-${1:-1}/)" }'
 compdef mpm=mpv_hack
 alias mpn='() { eval mp "*(.om[1,${1:-100}])" }'
