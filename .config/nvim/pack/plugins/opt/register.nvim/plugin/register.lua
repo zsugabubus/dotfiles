4c6b33f3d9f27ea5e23921e282cb2db6b99fc110
zsugabubus <zsugabubus@users.noreply.github.com>
2024-06-27 09:19:38 +0200

    nvim/register: Disable modeline

--- .config/nvim/pack/plugins/opt/register.nvim/plugin/register.lua
+++ .config/nvim/pack/plugins/opt/register.nvim/plugin/register.lua
@@ -1,34 +1,31 @@
-local group = vim.api.nvim_create_augroup('register', {})
+local api = vim.api
 
-vim.api.nvim_create_user_command('Register', function(opts)
+local group = api.nvim_create_augroup('register', {})
+
+api.nvim_create_user_command('Register', function(opts)
 	vim.cmd.edit(vim.fn.fnameescape('reg://' .. opts.args))
-end, {
-	nargs = '?',
-})
+end, { nargs = '?' })
 
-vim.api.nvim_create_autocmd('BufReadCmd', {
+api.nvim_create_autocmd('BufReadCmd', {
 	group = group,
 	pattern = 'reg://*',
 	callback = function(opts)
 		local regname = string.sub(opts.match, 7)
 		local lines = vim.split(vim.fn.getreg(regname, 1, false), '\n')
-		vim.api.nvim_buf_set_lines(opts.buf, 0, -1, true, lines)
+		api.nvim_buf_set_lines(opts.buf, 0, -1, true, lines)
+		vim.bo.modeline = false
 	end,
 })
 
-vim.api.nvim_create_autocmd('BufWriteCmd', {
+api.nvim_create_autocmd('BufWriteCmd', {
 	group = group,
 	pattern = 'reg://*',
 	callback = function(opts)
 		local regname = string.sub(opts.match, 7)
-		local lines = vim.api.nvim_buf_get_lines(opts.buf, 0, -1, true)
+		local lines = api.nvim_buf_get_lines(opts.buf, 0, -1, true)
 		vim.fn.setreg(regname, table.concat(lines, '\n'))
-		vim.bo[opts.buf].modified = false
-		vim.api.nvim_echo({
-			{
-				string.format('Register %s written', regname),
-				'Normal',
-			},
-		}, true, {})
+		vim.bo.modified = false
+		local s = string.format('Register %s written', regname)
+		api.nvim_echo({ { s, 'Normal' } }, false, {})
 	end,
 })
--- .config/nvim/pack/plugins/opt/register.nvim/spec/register_spec.lua
+++ .config/nvim/pack/plugins/opt/register.nvim/spec/register_spec.lua
@@ -1,21 +1,28 @@
 local vim = create_vim()
 
 test(':Register', function()
-	vim.fn.setreg('x', 'a\n\nb\n')
+	vim.cmd.Register()
+	assert.same('reg://', vim.fn.expand('%'))
+
 	vim.cmd.Register('x')
-	vim:assert_lines({ 'a', '', 'b', '' })
+	assert.same('reg://x', vim.fn.expand('%'))
 end)
 
-test(':edit', function()
-	vim.cmd.Register('x')
-	vim.fn.setreg('x', 'a\n\nb\n')
-	vim.cmd.edit()
-	vim:assert_lines({ 'a', '', 'b', '' })
-end)
+test('reg://', function()
+	vim.fn.setreg('', 'a')
+	vim.cmd.edit('reg://')
+	vim:assert_lines({ 'a' })
 
-test(':write', function()
-	vim.cmd.Register('x')
-	vim:set_lines({ 'a', '', 'b', '' })
+	vim.fn.setreg('x', 'a\n\nb\n')
+	vim.cmd.edit('reg://x')
+	vim:assert_lines({ 'a', '', 'b', '' })
+
 	vim.cmd.write()
 	assert.same('a\n\nb\n', vim.fn.getreg('x'))
+
+	vim.fn.setreg('x', ' vim: a\n')
+	vim.cmd.edit()
+	vim:assert_lines({ ' vim: a', '' })
+
+	vim:assert_messages('')
 end)
