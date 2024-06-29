99abc59a495a735dd3e60f4969b83fbfe0175a4b
zsugabubus <zsugabubus@users.noreply.github.com>
2024-06-29 18:54:25 +0200

    nvim/git: Replace deprecated vim.health.report_*

--- .config/nvim/pack/plugins/opt/git.nvim/lua/git/health.lua
+++ .config/nvim/pack/plugins/opt/git.nvim/lua/git/health.lua
@@ -2,16 +2,16 @@ local M = {}
 
 local function check_executable(x)
 	if vim.fn.executable(x) == 1 then
-		vim.health.report_ok(('%s executable found'):format(x))
+		vim.health.ok(('%s executable found'):format(x))
 	else
-		vim.health.report_error(
+		vim.health.error(
 			('%s not found in search path or not executable'):format(x)
 		)
 	end
 end
 
 function M.check()
-	vim.health.report_start('git')
+	vim.health.start('git')
 	check_executable('git')
 end
 
