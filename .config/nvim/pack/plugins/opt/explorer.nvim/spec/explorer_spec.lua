fd864e943bf9e547e4c18e27c6e8ccc0b773c30b
zsugabubus <zsugabubus@users.noreply.github.com>
2024-07-21 15:04:07 +0200

    nvim/explorer: Improve tests

--- .config/nvim/pack/plugins/opt/explorer.nvim/spec/explorer_spec.lua
+++ .config/nvim/pack/plugins/opt/explorer.nvim/spec/explorer_spec.lua
@@ -1,62 +1,63 @@
-local vim = create_vim({ isolate = false })
+local vim = create_vim({ width = 200 })
 
 describe('dir', function()
-	test(':edit', function()
-		vim.cmd.edit('/')
-		assert.is_true(vim.fn.search('./.') == 0)
-		assert.is_true(vim.fn.search('^/bin/$') > 0)
-		assert.is_true(vim.fn.search('^/etc/$') > vim.fn.search('^/bin/$'))
-	end)
+	test('edit', function()
+		local dir = vim.fn.tempname()
+		local a = dir .. '/a'
+		local ac = a .. '/c'
+		local b = dir .. '/b'
+		local d = dir .. '/d'
+		vim.fn.mkdir(ac, 'p')
+		vim.fn.mkdir(d, 'p')
+		vim.fn.writefile({}, b)
 
-	test(':file', function()
-		vim.cmd.edit('/')
-		assert.is_true(vim.fn.search('^/etc/.*/$') == 0)
-		vim.cmd.file('/etc/')
-		assert.is_true(vim.fn.search('^/etc/.*/$') > 0)
-	end)
+		vim.cmd.edit(dir)
+		assert.same('directory', vim.bo.filetype)
+		vim:assert_lines({ a .. '/', b, d .. '/' })
 
-	test('filetype', function()
-		vim.cmd.edit('/')
-		assert.same('directory', vim.o.filetype)
-	end)
+		vim.cmd.file(dir .. '//')
+		vim:assert_lines({ a .. '/', ac .. '/', b, d .. '/' })
 
-	test('recursive', function()
-		vim.cmd.edit('/')
-		vim.cmd.file('/etc//')
-		assert.same('/etc//', vim.fn.bufname())
-		assert.is_true(vim.fn.search('^/etc//') == 0)
-		assert.is_true(vim.fn.search('^/etc/.*/.') > 0)
+		assert.error_matches(function()
+			vim.cmd.write()
+		end, 'Cannot write')
 	end)
 end)
 
-describe('https', function()
-	test(':edit', function()
-		vim.cmd.edit('https://example.com')
-		assert.is_true(vim.fn.search('Example Domain') > 0)
+describe('http', function()
+	local function mock_curl(response)
+		local dir = vim.fn.tempname()
+		local bin = dir .. '/curl'
+		vim.fn.mkdir(dir)
+		vim.fn.writefile({
+			'#!/bin/sh -eu',
+			'printf %s ' .. vim.fn.shellescape(response),
+		}, bin)
+		vim.fn.setfperm(bin, 'r-x------')
+		vim.fn.setenv('PATH', string.format('%s:%s', dir, vim.fn.getenv('PATH')))
+	end
+
+	test('edit', function()
+		vim.cmd.edit(vim.fn.fnameescape('https://example.com#[1-10000]'))
+		assert.True(vim.fn.search('Example Domain') > 0)
+
+		assert.error_matches(function()
+			vim.cmd.write()
+		end, 'Cannot write')
 	end)
 
-	test('filetype with no path', function()
-		vim.cmd.edit('http://a.c')
-		assert.same('', vim.o.filetype)
-	end)
+	test('filetype', function()
+		local function test_case(url, filetype)
+			vim.cmd.edit(vim.fn.fnameescape(url))
+			return assert.same(filetype, vim.bo.filetype)
+		end
 
-	test('filetype with slash path', function()
-		vim.cmd.edit('http://a.c/')
-		assert.same('', vim.o.filetype)
-	end)
+		mock_curl('<!doctype html>')
 
-	test('filetype with path', function()
-		vim.cmd.edit('http://0.0.0.0//a.c')
-		assert.same('c', vim.o.filetype)
-	end)
-
-	test('filetype with query string', function()
-		vim.cmd.edit('http://0.0.0.0/a.json?/a.c')
-		assert.same('json', vim.o.filetype)
-	end)
-
-	test('filetype with fragment', function()
-		vim.cmd.edit('http://0.0.0.0/a.json\\#/a.c')
-		assert.same('json', vim.o.filetype)
+		test_case('http://a.c', 'html')
+		test_case('http://b.c/', 'html')
+		test_case('http://0.0.0.0//a.c', 'c')
+		test_case('http://0.0.0.0/a.json?/a.c', 'json')
+		test_case('http://0.0.0.0/a.json#/a.c', 'json')
 	end)
 end)
