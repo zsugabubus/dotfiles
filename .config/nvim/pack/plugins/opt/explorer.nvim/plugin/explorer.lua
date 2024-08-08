e06e39e5b8f2abc5a0694f8665429a7c1acf333c
zsugabubus <zsugabubus@users.noreply.github.com>
2024-08-08 15:53:37 +0200

    nvim/explorer: Fix escaping issues

--- .config/nvim/pack/plugins/opt/explorer.nvim/plugin/explorer.lua
+++ .config/nvim/pack/plugins/opt/explorer.nvim/plugin/explorer.lua
@@ -1,13 +1,41 @@
 local api = vim.api
+local cmd = vim.cmd
 local fn = vim.fn
+local uv = vim.uv
+
+local autocmd = api.nvim_create_autocmd
 
 local group = api.nvim_create_augroup('explorer', {})
 
-local function is_dir(path)
-	return fn.isdirectory(path) ~= 0
+local function dir_empty()
+	-- Do nothing.
 end
 
-api.nvim_create_autocmd('BufEnter', {
+local function dir(path)
+	local handle = uv.fs_scandir(path)
+	if handle then
+		return uv.fs_scandir_next, handle
+	end
+	return dir_empty
+end
+
+local function find(t, base, limit)
+	if limit <= 0 then
+		return
+	end
+
+	for name, kind in dir(base == '' and '.' or base) do
+		local path = base .. name
+		if kind == 'directory' then
+			table.insert(t, path .. '/')
+			find(t, path .. '/', limit - 1)
+		else
+			table.insert(t, path)
+		end
+	end
+end
+
+autocmd('BufEnter', {
 	group = group,
 	nested = true,
 	callback = function(opts)
@@ -26,56 +54,52 @@ api.nvim_create_autocmd('BufEnter', {
 		end
 		b.loaded_explorer = true
 
-		if not is_dir(opts.match) then
+		if fn.isdirectory(opts.match) == 0 then
 			return
 		end
 
-		local edit = vim.cmd.edit
+		bo.buftype = 'nofile'
+		bo.swapfile = false
+		bo.modeline = false
+		bo.filetype = 'directory'
 
-		api.nvim_create_autocmd('BufReadCmd', {
+		autocmd('BufReadCmd', {
 			group = group,
 			buffer = 0,
 			nested = true,
 			callback = function(opts)
-				local dirpath = fn.fnamemodify(opts.match, ':p:.')
+				local root = fn.fnamemodify(opts.match, ':p:.')
 				local recursive = string.match(opts.file, '//$')
 
 				local lines = {}
-				for name in
-					vim.fs.dir(dirpath == '' and '.' or dirpath, {
-						depth = recursive and math.huge or 1,
-					})
-				do
-					local path = dirpath .. name
-					table.insert(lines, path .. (is_dir(path) and '/' or ''))
-				end
+				find(lines, root, recursive and math.huge or 1)
 				table.sort(lines)
-				api.nvim_buf_set_lines(0, 0, -1, true, lines)
 
-				bo.buftype = 'nofile'
-				bo.swapfile = false
-				bo.modeline = false
-				bo.filetype = 'directory'
+				api.nvim_buf_set_lines(0, 0, -1, true, lines)
 			end,
 		})
 
-		api.nvim_create_autocmd({ 'DirChanged', 'BufFilePost' }, {
+		autocmd({ 'DirChanged', 'BufFilePost' }, {
 			buffer = 0,
 			nested = true,
 			callback = function()
-				edit()
+				cmd.edit()
 			end,
 		})
 
 		local keymap = api.nvim_buf_set_keymap
 		keymap(0, 'n', '<Plug>(explorer-goto-parent)', '', {
 			callback = function()
-				edit(fn.expand('%:p:h:h'))
+				cmd.edit(fn.fnameescape(fn.expand('%:p:h:h')))
 			end,
 		})
-		keymap(0, 'n', '<Plug>(explorer-recursive)', ':file %/<CR>', {})
+		keymap(0, 'n', '<Plug>(explorer-recursive)', ':file %//<CR>', {
+			callback = function()
+				cmd.file(fn.fnameescape(string.gsub(fn.expand('%'), '/+$', '') .. '//'))
+			end,
+		})
 		keymap(0, 'n', '<Plug>(explorer-cd)', ':cd <C-r>%<CR>:edit .<CR>', {})
 
-		edit()
+		cmd.edit()
 	end,
 })
--- .config/nvim/pack/plugins/opt/explorer.nvim/spec/explorer_spec.lua
+++ .config/nvim/pack/plugins/opt/explorer.nvim/spec/explorer_spec.lua
@@ -1,26 +1,96 @@
 local vim = create_vim({ width = 200 })
 
+local function tempname()
+	return vim.fn.tempname()
+		.. [[$PATH $(false) * % # | <cword> vim: a 'b" `false`]]
+end
+
 describe('dir', function()
+	test('non-directory', function()
+		local function f()
+			return vim.bo.filetype == 'directory'
+		end
+
+		local dir = tempname()
+		vim.fn.mkdir(dir, 'p')
+
+		vim.cmd.edit(vim.fn.fnameescape(dir))
+		assert.True(f())
+
+		local a = dir .. '/a'
+		vim.fn.writefile({}, a)
+		vim.cmd.edit(vim.fn.fnameescape(a))
+		assert.False(f())
+
+		vim.cmd.edit(vim.fn.fnameescape(dir .. '/newfile'))
+		assert.False(f())
+	end)
+
 	test('edit', function()
-		local dir = vim.fn.tempname()
+		local dir = tempname()
 		local a = dir .. '/a'
 		local ac = a .. '/c'
+		local acc = ac .. '/c'
 		local b = dir .. '/b'
 		local d = dir .. '/d'
-		vim.fn.mkdir(ac, 'p')
+		vim.fn.mkdir(acc, 'p')
 		vim.fn.mkdir(d, 'p')
 		vim.fn.writefile({}, b)
 
-		vim.cmd.edit(dir)
+		vim.cmd.edit(vim.fn.fnameescape(dir))
 		assert.same('directory', vim.bo.filetype)
 		vim:assert_lines({ a .. '/', b, d .. '/' })
 
-		vim.cmd.file(dir .. '//')
-		vim:assert_lines({ a .. '/', ac .. '/', b, d .. '/' })
+		vim.cmd.file(vim.fn.fnameescape(dir .. '//'))
+		vim:assert_lines({ a .. '/', ac .. '/', acc .. '/', b, d .. '/' })
 
 		assert.error_matches(function()
 			vim.cmd.write()
 		end, 'Cannot write')
+	end)
+
+	test(':chdir', function()
+		local dir = vim.fn.tempname()
+		local a = dir .. '/a'
+		vim.fn.mkdir(a, 'p')
+
+		vim.cmd.edit(dir)
+		vim:assert_lines({ a .. '/' })
+
+		vim.fn.chdir(dir)
+		vim:assert_lines({ 'a/' })
+	end)
+
+	test('<Plug>(explorer-goto-parent)', function()
+		local dir = tempname()
+		local a = dir .. '/a'
+		local ab = a .. '/b'
+		vim.fn.mkdir(ab, 'p')
+
+		vim.keymap.set('', 's', '<Plug>(explorer-goto-parent)')
+		vim.cmd.edit(vim.fn.fnameescape(ab))
+
+		vim:feed('s')
+		assert.same(a, vim.fn.bufname())
+
+		vim:feed('s')
+		assert.same(dir, vim.fn.bufname())
+	end)
+
+	test('<Plug>(explorer-recursive)', function()
+		local dir = tempname()
+		vim.fn.mkdir(dir, 'p')
+
+		vim.keymap.set('', 's', '<Plug>(explorer-recursive)')
+		vim.cmd.edit(vim.fn.fnameescape(dir))
+		local bufnr = vim.fn.bufnr()
+
+		vim:feed('s')
+		assert.same(bufnr, vim.fn.bufnr())
+		assert.same(dir .. '//', vim.fn.bufname())
+
+		vim:feed('s')
+		assert.same(dir .. '//', vim.fn.bufname())
 	end)
 end)
 
@@ -83,8 +153,7 @@ describe('ssh', function()
 
 	before_each(function()
 		mock_ssh()
-		root = vim.fn.tempname()
-			.. [[$PATH $(false) * % # <cword> vim: a 'b" `false`]]
+		root = tempname()
 		vim.fn.mkdir(root)
 	end)
 
