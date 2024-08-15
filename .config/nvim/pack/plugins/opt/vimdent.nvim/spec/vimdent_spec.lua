0323404bb8a7dec560d421ab786d18081c25b791
zsugabubus <zsugabubus@users.noreply.github.com>
2024-08-15 15:58:05 +0200

    nvim/vimdent: Improve tests

--- .config/nvim/pack/plugins/opt/vimdent.nvim/spec/vimdent_spec.lua
+++ .config/nvim/pack/plugins/opt/vimdent.nvim/spec/vimdent_spec.lua
@@ -1,6 +1,8 @@
 local vim = create_vim({ isolate = false, height = 20 })
 
-test('options unchanged when detection fails', function()
+local DOUBLE_SPACE = { 'a', '  b', '    c' }
+
+it('does not change options when detection fails', function()
 	vim.cmd('setlocal tabstop=42')
 	vim.cmd.Vimdent()
 
@@ -8,7 +10,7 @@ test('options unchanged when detection fails', function()
 	assert.same(42, vim.bo.tabstop)
 end)
 
-test('buffer with single-space indentation ignored', function()
+it('ignores single-space indentation', function()
 	vim:set_lines({ 'a', ' b', '  c' })
 	vim.cmd.Vimdent()
 
@@ -18,8 +20,8 @@ test('buffer with single-space indentation ignored', function()
 	assert.same(8, vim.bo.shiftwidth)
 end)
 
-test('buffer with double-space indentation correctly detected', function()
-	vim:set_lines({ 'a', '  b', '    c' })
+it('detects double-space indentation', function()
+	vim:set_lines(DOUBLE_SPACE)
 	vim.cmd.Vimdent()
 
 	assert.same(1, vim.b.did_vimdent)
@@ -28,7 +30,7 @@ test('buffer with double-space indentation correctly detected', function()
 	assert.same(2, vim.bo.shiftwidth)
 end)
 
-test('buffer with tabs correctly detected', function()
+it('detects tab-only indentation', function()
 	vim:set_lines({ 'a', '\tb', '\t\tc' })
 	vim.cmd.Vimdent()
 
@@ -38,54 +40,80 @@ test('buffer with tabs correctly detected', function()
 	assert.same(0, vim.bo.shiftwidth)
 end)
 
-test(
-	'buffer with mixed space and tab indentation correctly detected',
-	function()
-		vim:set_lines({ 'a', '  b', '\tc' })
-		vim.cmd.Vimdent()
+it('detects mixed space and tab indentation', function()
+	vim:set_lines({ 'a', '  b', '\tc' })
+	vim.cmd.Vimdent()
 
-		assert.same(1, vim.b.did_vimdent)
-		assert.same(4, vim.bo.tabstop)
-		assert.same(false, vim.bo.expandtab)
-		assert.same(2, vim.bo.shiftwidth)
-	end
-)
-
-test('options re-detected on &filetype set', function()
-	vim.cmd.new('test')
-	vim:set_lines({ 'a', '  b', '    c' })
-
-	vim.cmd('setlocal filetype=c')
 	assert.same(1, vim.b.did_vimdent)
-
-	-- Once only.
-	vim.b.did_vimdent = nil
-	vim.cmd('setlocal filetype=c')
-	assert.same(nil, vim.b.did_vimdent)
+	assert.same(4, vim.bo.tabstop)
+	assert.same(false, vim.bo.expandtab)
+	assert.same(2, vim.bo.shiftwidth)
 end)
 
-test('options re-detected on :write', function()
-	vim.cmd.new('test')
-	vim:set_lines({ 'a', '  b', '    c' })
+it('ignores non-file buffers', function()
+	vim:set_lines(DOUBLE_SPACE)
 
-	vim.cmd('doautocmd BufWritePost')
-	assert.same(1, vim.b.did_vimdent)
-
-	-- Once only.
-	vim.b.did_vimdent = nil
-	vim.cmd('doautocmd BufWritePost')
-	assert.same(nil, vim.b.did_vimdent)
-end)
-
-test('options detected only in file-backed buffers', function()
 	vim.bo.buftype = 'quickfix'
-	vim:set_lines({ 'a', '  b', '    c' })
 	vim.cmd.Vimdent()
 
 	assert.same(nil, vim.b.did_vimdent)
+
+	vim.bo.buftype = ''
+	vim.cmd.Vimdent()
+
+	assert.same(1, vim.b.did_vimdent)
 end)
 
-test('&filetype fallback copies options from detected buffer', function()
+it('detects indentation on BufNewFile', function()
+	vim:set_lines(DOUBLE_SPACE)
+
+	vim.cmd('doautocmd BufNewFile')
+	assert.same(1, vim.b.did_vimdent)
+end)
+
+it('detects indentation on BufReadPost', function()
+	vim:set_lines(DOUBLE_SPACE)
+
+	vim.cmd('doautocmd BufReadPost')
+	assert.same(nil, vim.b.did_vimdent)
+	vim.cmd('doautocmd BufEnter')
+	assert.same(1, vim.b.did_vimdent)
+
+	-- Once only.
+	vim.b.did_vimdent = nil
+	vim.cmd('doautocmd BufEnter')
+	assert.same(nil, vim.b.did_vimdent)
+end)
+
+it('detects indentation on FileType', function()
+	vim.cmd.new('new')
+	vim:set_lines(DOUBLE_SPACE)
+
+	assert.same(nil, vim.b.did_vimdent)
+	vim.cmd('setlocal filetype=c')
+	assert.same(1, vim.b.did_vimdent)
+
+	-- Once only.
+	vim.b.did_vimdent = nil
+	vim.cmd('setlocal filetype=c')
+	assert.same(nil, vim.b.did_vimdent)
+end)
+
+it('detects indentation on BufWritePost', function()
+	vim.cmd.new('new')
+	vim:set_lines(DOUBLE_SPACE)
+
+	assert.same(nil, vim.b.did_vimdent)
+	vim.cmd('doautocmd BufWritePost')
+	assert.same(1, vim.b.did_vimdent)
+
+	-- Once only.
+	vim.b.did_vimdent = nil
+	vim.cmd('doautocmd BufWritePost')
+	assert.same(nil, vim.b.did_vimdent)
+end)
+
+it('copies options from other buffer based on filetype', function()
 	vim.cmd([[
 new ok_but_not_c
 setlocal filetype=not_c tabstop=99
@@ -95,19 +123,23 @@ new not_ok
 setlocal filetype=c tabstop=99
 
 new first
-setlocal filetype=c tabstop=42
+setlocal filetype=c tabstop=16
 let b:did_vimdent = 1
 
 new second_ignored
 setlocal filetype=c tabstop=99
 let b:did_vimdent = 1
-
-new test
 	]])
 
+	vim.cmd.new('test')
 	assert.same(8, vim.bo.tabstop)
+	assert.same(nil, vim.b.did_vimdent)
 
 	vim.cmd('setlocal filetype=c')
+	assert.same(16, vim.bo.tabstop)
+	assert.same(nil, vim.b.did_vimdent)
 
-	assert.same(42, vim.bo.tabstop)
+	vim.cmd.new('test.c')
+	assert.same(16, vim.bo.tabstop)
+	assert.same(nil, vim.b.did_vimdent)
 end)
