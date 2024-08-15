79963f4fc43b29ccf28813ec3782ae52bedcc6b0
zsugabubus <zsugabubus@users.noreply.github.com>
2024-08-15 16:05:22 +0200

    nvim/archive: Improve tests

--- .config/nvim/pack/plugins/opt/archive.nvim/spec/archive_spec.lua
+++ .config/nvim/pack/plugins/opt/archive.nvim/spec/archive_spec.lua
@@ -1,4 +1,4 @@
-local vim = create_vim({ isolate = false })
+local vim = create_vim({ isolate = false, width = 200 })
 
 local function system(cmd)
 	local output = vim.fn.system(cmd)
@@ -11,73 +11,91 @@ end
 local CONTENT = { 'foo', 'bar', 'baz' }
 local OTHER_CONTENT = { 'hello world' }
 
+local function tempname()
+	return vim.fn.tempname() .. [[$PATH $(false) '"` % * # vim: a |]]
+end
+
 describe('zip', function()
 	local c_path
 	local foo_path
 	local zip_path
 
+	local function edit_deleted_archive()
+		it(':edit shows stderr when archive does not exist', function()
+			vim.fn.delete(zip_path)
+			vim.cmd.edit()
+			assert.matches('cannot find or open', vim.fn.getline(1))
+			assert.same('nofile', vim.bo.buftype)
+			assert.same('', vim.bo.filetype)
+			assert.False(vim.bo.modeline)
+		end)
+	end
+
 	before_each(function()
-		local prefix = vim.fn.tempname()
+		local prefix = tempname()
 		c_path = prefix .. '.c'
 		foo_path = prefix .. 'foo'
 		zip_path = prefix .. '.zip'
 
+		vim.cmd.edit(vim.fn.fnameescape(zip_path))
+
 		vim.fn.writefile(CONTENT, c_path)
 		vim.fn.writefile({}, foo_path)
-
 		system({ 'zip', zip_path, c_path })
 
-		vim.cmd.edit(vim.fn.fnameescape(zip_path))
+		vim.cmd.edit()
 	end)
 
-	describe('list', function()
-		test('rendered', function()
+	describe('list view', function()
+		it('shows correct members', function()
 			vim:assert_lines({ string.sub(c_path, 2) })
+			assert.same('nofile', vim.bo.buftype)
+			assert.same('archive', vim.bo.filetype)
+			assert.False(vim.bo.modeline)
 		end)
 
-		test(':edit', function()
+		it(':edit reloads members', function()
 			system({ 'zip', zip_path, foo_path })
 			vim.cmd.edit()
 			vim:assert_lines({ string.sub(c_path, 2), string.sub(foo_path, 2) })
 		end)
 
-		test('gf', function()
+		edit_deleted_archive()
+
+		it('gf goes to member', function()
 			local bufnr = vim.fn.bufnr()
 			vim:feed('gf')
 			assert.are_not.same(bufnr, vim.fn.bufnr())
 		end)
 
-		test('<CR>', function()
+		it('<CR> goes to member', function()
 			local bufnr = vim.fn.bufnr()
 			vim:feed('\r')
 			assert.are_not.same(bufnr, vim.fn.bufnr())
 		end)
 	end)
 
-	describe('file', function()
+	describe('member view', function()
 		before_each(function()
 			vim:feed('gf')
 		end)
 
-		test('content', function()
+		it('shows correct content', function()
 			vim:assert_lines(CONTENT)
-		end)
-
-		test('&filetype', function()
+			assert.same('nofile', vim.bo.buftype)
 			assert.same('c', vim.bo.filetype)
+			assert.True(vim.bo.modeline)
 		end)
 
-		test(':edit', function()
+		it(':edit reloads content', function()
 			vim.fn.writefile(OTHER_CONTENT, c_path)
 			system({ 'zip', zip_path, c_path })
+			vim:assert_lines(CONTENT)
 			vim.cmd.edit()
 			vim:assert_lines(OTHER_CONTENT)
 		end)
 
-		test(':write', function()
-			assert.same('nofile', vim.bo.buftype)
-			assert.has.error(vim.cmd.write)
-		end)
+		edit_deleted_archive()
 	end)
 end)
 
@@ -86,36 +104,53 @@ describe('xz', function()
 	local xz_path
 
 	before_each(function()
-		local prefix = vim.fn.tempname()
+		local prefix = tempname()
 		input_path = prefix .. '.c'
 		xz_path = input_path .. '.xz'
+
+		vim.cmd.edit(vim.fn.fnameescape(xz_path))
 
 		vim.fn.writefile(CONTENT, input_path)
 		system({ 'xz', input_path })
 
-		vim.cmd.edit(vim.fn.fnameescape(xz_path))
+		vim.cmd.edit()
 	end)
 
-	test('content', function()
+	it('shows correct content', function()
 		vim:assert_lines(CONTENT)
-	end)
-
-	test('&filetype', function()
 		assert.same('c', vim.bo.filetype)
+		assert.True(vim.bo.modeline)
 	end)
 
-	test(':edit', function()
+	it(':edit reloads content', function()
 		vim.fn.writefile(OTHER_CONTENT, input_path)
 		vim.fn.delete(xz_path)
 		system({ 'xz', input_path })
+		vim:assert_lines(CONTENT)
 		vim.cmd.edit()
 		vim:assert_lines(OTHER_CONTENT)
 	end)
 
-	test(':write', function()
+	it(':edit shows stderr when file does not exist', function()
+		vim.fn.delete(xz_path)
+		vim.cmd.edit()
+		assert.matches('xz:.*No such file or directory', vim.fn.getline(1))
+		assert.same('nofile', vim.bo.buftype)
+		assert.same('', vim.bo.filetype)
+		assert.False(vim.bo.modeline)
+	end)
+
+	it(':write checks read-only', function()
+		vim.fn.setfperm(xz_path, '---------')
+		assert.error_matches(vim.cmd.write, 'E505.*is read%-only')
+		assert.False(vim.bo.modified)
+	end)
+
+	it(':write writes compressed file', function()
+		vim.fn.setfperm(xz_path, '---------')
 		vim:set_lines(OTHER_CONTENT)
 		assert.True(vim.bo.modified)
-		vim.cmd.write()
+		vim.cmd.write({ bang = true })
 		assert.False(vim.bo.modified)
 		assert.same(
 			table.concat(OTHER_CONTENT, '\n') .. '\n',
