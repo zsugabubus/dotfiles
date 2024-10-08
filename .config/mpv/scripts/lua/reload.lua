c80493c50c09df59bf733352a4f75283d3ca7703
zsugabubus <zsugabubus@users.noreply.github.com>
2024-10-09 01:35:48 +0200

    mpv/reload: Keep other files

--- .config/mpv/scripts/lua/reload.lua
+++ .config/mpv/scripts/lua/reload.lua
@@ -1,11 +1,19 @@
 mp.add_key_binding('R', 'reload-file', function()
 	local path = mp.get_property('path')
-	if path ~= nil then
-		mp.commandv(
-			'loadfile',
-			path,
-			'replace',
-			'start=' .. mp.get_property_number('time-pos')
-		)
+	if not path then
+		return
 	end
+
+	local current = mp.get_property_native('playlist-playing-pos')
+	local last = mp.get_property('playlist-count')
+
+	mp.commandv(
+		'loadfile',
+		path,
+		'append',
+		'start=' .. mp.get_property_number('time-pos')
+	)
+	mp.commandv('playlist-play-index', last)
+	mp.commandv('playlist-move', last, current + 1)
+	mp.commandv('playlist-remove', current)
 end, { repeatable = false })
