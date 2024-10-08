4eea94080e4e2a067b179f34deb00dcad81617b9
zsugabubus <zsugabubus@users.noreply.github.com>
2024-10-09 00:59:20 +0200

    mpv/title: Change path shortening

--- .config/mpv/scripts/lua/title.lua
+++ .config/mpv/scripts/lua/title.lua
@@ -4,41 +4,48 @@ local SPACE_PATTERNS = { '%.', '%-', '%_' }
 
 local M = {}
 
-local cache = setmetatable({}, { __mode = 'kv' })
+local cwd = require('mp.utils').getcwd() .. '/'
+local filename_to_title = setmetatable({}, {
+	__mode = 'kv',
+	__index = function(t, k)
+		local s = k
 
-function M.from_playlist_entry(entry)
-	local s = cache[entry.filename]
-	if s then
-		return s
-	end
+		if string.sub(s, 1, 2) == './' then
+			s = string.sub(s, 3)
+		elseif string.sub(s, 1, #cwd) == cwd then
+			s = string.sub(s, #cwd + 1)
+		end
 
-	local s = entry.filename
-	s = string.gsub(s, '^./', '')
-	s = string.gsub(s, '/.*/', '/\u{2026}/')
-	s = string.gsub(s, '([^/])%.[0-9A-Za-z]+$', '%1')
+		s = string.gsub(s, '([^/])%.[0-9A-Za-z]+$', '%1')
 
-	if not string.find(s, ' ') then
-		local n = 0
-		for _, pattern in pairs(SPACE_PATTERNS) do
-			local x, xn = string.gsub(s, pattern, ' ')
-			if xn > n then
-				s, n = x, xn
+		if not string.find(s, ' ', 1, true) then
+			local n = 0
+			for _, pattern in pairs(SPACE_PATTERNS) do
+				local x, xn = string.gsub(s, pattern, ' ')
+				if xn > n then
+					s, n = x, xn
+				end
 			end
 		end
-	end
 
-	s = string.gsub(s, ' [0-9]+p[^/]*', '')
-	s = string.gsub(s, ' [1-9][0-9][0-9][0-9] [A-Za-z0-9][^/]', '')
+		s = string.gsub(s, ' [0-9]+p[^/]*', '')
+		s = string.gsub(s, ' [1-9][0-9][0-9][0-9] [A-Za-z0-9][^/]', '')
 
-	s = Osd.esc(s)
-	cache[entry.filename] = s
-	return s
+		s = Osd.esc(s)
+		t[k] = s
+		return s
+	end,
+})
+
+function M.from_playlist_entry(entry)
+	return filename_to_title[entry.filename]
 end
 
 function M.get_current_ass()
 	local artist = mp.get_property_native('metadata/by-key/Artist', nil)
 	local title = mp.get_property_native('metadata/by-key/Title', nil)
 		or mp.get_property_native('media-title', nil)
+
 	if artist and title then
 		local version = mp.get_property_native('metadata/by-key/Version', nil)
 		return table.concat({
@@ -51,18 +58,17 @@ function M.get_current_ass()
 			version and Osd.esc(version),
 			version and ')',
 		})
+	end
+
+	local pos = mp.get_property_native('playlist-pos')
+	local current = mp.get_property_native('playlist/' .. pos)
+
+	if current and (not title or title == current.filename:gsub('^.*/', '')) then
+		return M.from_playlist_entry(current)
+	elseif title then
+		return Osd.esc(title)
 	else
-		local pos = mp.get_property_native('playlist-pos')
-		local current = mp.get_property_native('playlist/' .. pos)
-		if
-			current and (not title or title == current.filename:gsub('^.*/', ''))
-		then
-			return M.from_playlist_entry(current)
-		elseif title then
-			return Osd.esc(title)
-		else
-			return
-		end
+		return
 	end
 end
 
