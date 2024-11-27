e10ad721b43df89d6beb234eda57542d6a915008
zsugabubus <zsugabubus@users.noreply.github.com>
2024-11-27 11:41:40 +0100

    mpv/here: Write when playback starts

--- .config/mpv/scripts/lua/here.lua
+++ .config/mpv/scripts/lua/here.lua
@@ -2,6 +2,7 @@ local utils = require('mp.utils')
 
 local cwd = utils.getcwd()
 local mpv_here = os.getenv('XDG_RUNTIME_DIR') .. '/mpv_here'
+local path
 
 local function join_url(a, b)
 	if string.sub(b, 1, 1) == '/' or string.find(b, '^[a-z]+://') then
@@ -10,16 +11,24 @@ local function join_url(a, b)
 	return a .. '/' .. b
 end
 
-mp.observe_property('path', 'native', function(_, path)
+local function write()
 	if not path then
 		return
 	end
-
 	local f = io.open(mpv_here, 'w')
-	if not f then
-		return
+	if f then
+		assert(f:write(join_url(cwd, path)))
+		assert(f:close())
 	end
+end
 
-	assert(f:write(join_url(cwd, path)))
-	assert(f:close())
+mp.observe_property('path', 'native', function(_, x)
+	path = x
+	write()
+end)
+
+mp.observe_property('pause', 'native', function(_, x)
+	if not x then
+		write()
+	end
 end)
