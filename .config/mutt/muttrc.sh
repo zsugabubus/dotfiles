cec5695390e9f13cf9ca42c68748e9e3ea968499
zsugabubus <zsugabubus@users.noreply.github.com>
2025-03-05 02:06:23 +0100

    mutt: Fix space in folder names

--- .config/mutt/muttrc.sh
+++ .config/mutt/muttrc.sh
@@ -14,7 +14,7 @@ cd -- "$MAIL"
 
 echo 'unmailboxes *'
 
-echo 'set spoolfile=+'"$(readlink .spool)"
+echo "set spoolfile='+$(readlink .spool)'"
 
 # Indentation have to be manually crafted since Mutt's method works only
 # if we inserted a dummy x mailbox that is simply a waste of space. This
@@ -24,28 +24,28 @@ for dir in */inbox */cur; do
 	maildir=${dir%/*}
 
 	case $dir in
-	*/cur) echo "mailboxes =$maildir" ;;
-	*) echo "mailboxes -label $maildir =$maildir/inbox" ;;
+	*/cur) echo "mailboxes '=$maildir'" ;;
+	*) echo "mailboxes -label '$maildir' '=$maildir/inbox'" ;;
 	esac
 
 	for subdir in "$maildir/"*/; do
 		subdir=${subdir%/}
 		case $subdir in
 		*/cur|*/new|*/tmp|*/inbox) ;;
-		*) echo "mailboxes -label '  ${subdir#*/}' =$subdir" ;;
+		*) echo "mailboxes -label '  ${subdir#*/}' '=$subdir'" ;;
 		esac
 	done
 
 	muttrc=$maildir/muttrc
 	if test -f "$muttrc"; then
-		echo "folder-hook $maildir 'source =$muttrc'"
+		echo "folder-hook '$maildir' source '=$muttrc'"
 	fi
 done
 
 for signature in */signature; do
 	test "$signature" = "${signature#'*/'}" || continue
 	mgroup=${signature%/*}
-	echo "folder-hook $mgroup 'set signature==$signature'"
+	echo "folder-hook $mgroup set signature='=$signature'"
 done
 
 for maildir in */cur */inbox; do
