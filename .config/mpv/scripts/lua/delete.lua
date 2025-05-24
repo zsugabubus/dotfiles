90a0f34ac991ae633388dfab7a6ce5ca39903a6b
zsugabubus <zsugabubus@users.noreply.github.com>
2025-05-24 11:52:02 +0200

    mpv/delete: Delete related files

--- .config/mpv/scripts/lua/delete.lua
+++ .config/mpv/scripts/lua/delete.lua
@@ -1,5 +1,44 @@
 local utils = require('mp.utils')
 
+local function append_to_deleted_log(path)
+	local f = assert(io.open('/tmp/mpv_deleted', 'a'))
+	assert(f:write(path .. '\n'))
+	assert(f:close())
+end
+
+local function move_to_deleted(path, deleted_ext)
+	append_to_deleted_log(path)
+	local dirname = utils.split_path(path)
+	local deleted_path = utils.join_path(dirname, '.deleted' .. deleted_ext)
+	local ok, err = os.rename(path, deleted_path)
+	if not ok then
+		mp.msg.error(err)
+		return false
+	end
+	mp.msg.info(('Moved: %s -> %s'):format(path, deleted_path))
+	return true
+end
+
+local function can_delete_extension(s)
+	return s:match('^%.[a-z]+%.vtt$')
+end
+
+local function delete_related_paths(path)
+	local dirname, thisfile = utils.split_path(path)
+	local prefix = thisfile:match('(.+)%.[^.]+$')
+	if not prefix then
+		return
+	end
+	for _, file in ipairs(utils.readdir(dirname, 'files')) do
+		if thisfile ~= file and file:find(prefix, 1, true) == 1 then
+			local ext = file:sub(#prefix + 1)
+			if can_delete_extension(ext) then
+				move_to_deleted(utils.join_path(dirname, file), ext)
+			end
+		end
+	end
+end
+
 mp.add_key_binding(nil, 'delete-file', function()
 	local path = mp.get_property_native('path')
 	if not path then
@@ -21,26 +60,12 @@ mp.add_key_binding(nil, 'delete-file', function()
 		return
 	end
 
-	local dirname, _ = utils.split_path(path)
+	delete_related_paths(path)
 
-	local log_path = utils.join_path(dirname, '.deleted.log')
-	local f, err = io.open(log_path, 'a')
-	if not f then
-		mp.msg.error(err)
-		mp.osd_message('Delete failed')
-		return
-	end
-	f:write(path .. '\n')
-	f:close()
-
-	local new_path = utils.join_path(dirname, '.deleted')
-	local ok, err = os.rename(path, new_path)
-	if ok then
-		mp.msg.info(('Moved: %s -> %s'):format(path, new_path))
+	if move_to_deleted(path, '') then
 		mp.osd_message('File deleted')
 		mp.command('playlist_remove current')
 	else
-		mp.msg.error(err)
 		mp.osd_message('Delete failed')
 	end
 end, { repeatable = false })
